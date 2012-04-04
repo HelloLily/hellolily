7384f7e817f92c61ed44a3c019b0a1ce9dbf23ef
snoepkast <allard@solidsquare.nl>
2012-04-04 17:32:51 +0200

    Project is now ready to run on Heroku, instead of local setting files we now use os_environ variables, also added a placeholder for 404 and 500 pages

--- lily/settings.py
+++ lily/settings.py
@@ -1,13 +1,35 @@
-from django.core.exceptions import ImproperlyConfigured
-from django.core.urlresolvers import reverse_lazy
 import os
-import site_settings
+from urlparse import urlparse, uses_netloc
+from django.core.urlresolvers import reverse_lazy
 
-DEBUG = TEMPLATE_DEBUG = False
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+local_path = lambda path: os.path.join(os.path.dirname(__file__), path)
+uses_netloc.append('postgres')
+uses_netloc.append('redis')
+
+DEBUG = os.environ.get('DEBUG', False)
+DEV = os.environ.get('DEV', False)
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
+)
+
+MANAGERS = ADMINS
+
+if DEV: # Only use sqlite db when in dev mode, Heroku injects db settings on deployment
+    DATABASES = { # Check: https://docs.djangoproject.com/en/dev/ref/settings/#databases
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'local.sqlite',
+            'USER': '',
+            'PASSWORD': '',
+            'HOST': '',
+            'PORT': '',
+        }
+    }
 
 TIME_ZONE = 'Europe/Amsterdam'
-LANGUAGE_CODE = 'En-en' # 'NL-nl'
+LANGUAGE_CODE = 'NL-nl'
 
 SITE_ID = 1
 
@@ -16,23 +38,26 @@ USE_L10N = True
 USE_TZ = True
 FIRST_DAY_OF_WEEK = 1
 
-CSRF_COOKIE_SECURE = True
+# Don't share this with anybody.
+SECRET_KEY = os.environ.get('SECRET_KEY', 'my-secret-key')
+
+CSRF_COOKIE_SECURE = False #For production this needs to be set to True
 CSRF_FAILURE_VIEW = 'django.views.csrf.csrf_failure'
 
-SESSION_COOKIE_SECURE = True
-SESSION_COOKIE_HTTPONLY = True
-SESSION_EXPIRE_AT_BROWSER_CLOSE = True
-X_FRAME_OPTIONS = 'SAMEORIGIN'
+SESSION_COOKIE_SECURE = False #For production this needs to be set to True
+SESSION_COOKIE_HTTPONLY = False #For production this needs to be set to True
+SESSION_EXPIRE_AT_BROWSER_CLOSE = False
+X_FRAME_OPTIONS = 'SAMEORIGIN' #For production this needs to be set to DENY
 
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.environ.get('MEDIA_ROOT', local_path('media/'))
+MEDIA_URL = os.environ.get('MEDIA_URL', '/media/')
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static_collected')
-STATIC_URL = '/static/'
+STATIC_ROOT = os.environ.get('STATIC_ROOT', local_path('static_collected/'))
+STATIC_URL = os.environ.get('STATIC_URL', '/static/')
 
 #LOGIN STUFF
 LOGIN_URL = reverse_lazy('login')
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = reverse_lazy('dashboard')
 LOGOUT_URL = reverse_lazy('logout')
 AUTHENTICATION_BACKENDS = (
     'lily.users.auth_backends.UserModelBackend',
@@ -42,7 +67,7 @@ USER_INVITATION_TIMEOUT_DAYS = 7
 CUSTOM_USER_MODEL = 'users.UserModel'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    local_path('static/'),
     # Optional: see https://docs.djangoproject.com/en/1.3/ref/contrib/staticfiles/#prefixes-optional
 )
 
@@ -56,10 +81,18 @@ FILE_UPLOAD_HANDLERS = (
     'django.core.files.uploadhandler.TemporaryFileUploadHandler',
 )
 
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-)
+if not DEBUG:
+    TEMPLATE_LOADERS = (
+        ('django.template.loaders.cached.Loader', (
+            'django.template.loaders.filesystem.Loader',
+            'django.template.loaders.app_directories.Loader',
+        )),
+    )
+else:
+    TEMPLATE_LOADERS = (
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    )
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
@@ -74,7 +107,7 @@ ROOT_URLCONF = 'lily.urls'
 WSGI_APPLICATION = 'lily.wsgi.application'
 
 TEMPLATE_DIRS = (
-    os.path.join(PROJECT_ROOT, 'templates')
+    local_path('templates/')
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
@@ -100,6 +133,7 @@ INSTALLED_APPS = (
     
     # 3rd party
     'templated_email',
+    'gunicorn',
     
     # Lily
     'lily',
@@ -109,6 +143,18 @@ INSTALLED_APPS = (
     'lily.users',
     'lily.utils',
 )
+
+# Settings for templated e-mail app
+TEMPLATED_EMAIL_TEMPLATE_DIR = 'email/'
+
+DEFAULT_FROM_EMAIL = 'lily@hellolily.com'
+SERVER_EMAIL = 'lily@hellolily.com'
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'lily@hellolily.com'
+EMAIL_HOST_PASSWORD = 'hellolily'
+EMAIL_PORT = 587
 
 LOGGING = {
     'version': 1,
@@ -134,21 +180,20 @@ LOGGING = {
     }
 }
 
-# Cache settings
-#CACHES = {
-#    'default': {
-#        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-#        'TIMEOUT': 300
+# TODO add the add-on on heroku first(!) and then enable this.
+
+# Cache configuration
+#if not DEBUG:
+#    url = urlparse(os.environ['REDISTOGO_URL'])
+#    CACHES = {
+#        'default': {
+#            'BACKEND': 'redis_cache.RedisCache',
+#            'LOCATION': "{0.hostname}:{0.port}".format(url),
+#            'OPTIONS': {
+#                'PASSWORD': url.password,
+#                'DB': 0
+#            }
+#        }
 #    }
-#}
 
-# Settings for templated e-mail app
-TEMPLATED_EMAIL_TEMPLATE_DIR = 'email/'
 
-try:
-    from site_settings import *
-except:
-    pass
-
-if not hasattr(site_settings, 'DEFAULT_FROM_EMAIL'):
-    raise ImproperlyConfigured('Missing DEFAULT_FROM_EMAIL in site_settings')

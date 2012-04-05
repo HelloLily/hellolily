7690988c282799ae185e38d41e0b46b110d4813b
snoepkast <allard@solidsquare.nl>
2012-04-05 15:29:04 +0200

    update, now we can override almost everything using environment variables

--- lily/settings.py
+++ lily/settings.py
@@ -2,16 +2,16 @@ import os
 from urlparse import urlparse, uses_netloc
 from django.core.urlresolvers import reverse_lazy
 
+boolean = lambda value: bool(int(value))
 local_path = lambda path: os.path.join(os.path.dirname(__file__), path)
 uses_netloc.append('postgres')
 uses_netloc.append('redis')
 
-DEBUG = os.environ.get('DEBUG', False)
-DEV = os.environ.get('DEV', False)
+DEBUG = boolean(os.environ.get('DEBUG', 0))
+DEV = boolean(os.environ.get('DEV', 0))
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = eval(os.environ.get('ADMINS', '()'))
-
 MANAGERS = ADMINS
 
 if DEV: # Only use sqlite db when in dev mode, Heroku injects db settings on deployment
@@ -29,23 +29,23 @@ if DEV: # Only use sqlite db when in dev mode, Heroku injects db settings on dep
 TIME_ZONE = 'Europe/Amsterdam'
 LANGUAGE_CODE = 'NL-nl'
 
-SITE_ID = 1
+SITE_ID = os.environ.get('SITE_ID', 1)
 
-USE_I18N = True
-USE_L10N = True
-USE_TZ = True
-FIRST_DAY_OF_WEEK = 1
+USE_I18N = boolean(os.environ.get('USE_I18N', 1))
+USE_L10N = boolean(os.environ.get('USE_L10N', 1))
+USE_TZ = boolean(os.environ.get('USE_TZ', 1))
+FIRST_DAY_OF_WEEK = os.environ.get('FIRST_DAY_OF_WEEK', 1)
 
 # Don't share this with anybody.
 SECRET_KEY = os.environ.get('SECRET_KEY', 'my-secret-key')
 
-CSRF_COOKIE_SECURE = False #For production this needs to be set to True
+CSRF_COOKIE_SECURE = boolean(os.environ.get('CSRF_COOKIE_SECURE', 0)) #For production this needs to be set to True
 CSRF_FAILURE_VIEW = 'django.views.csrf.csrf_failure'
 
-SESSION_COOKIE_SECURE = False #For production this needs to be set to True
-SESSION_COOKIE_HTTPONLY = False #For production this needs to be set to True
-SESSION_EXPIRE_AT_BROWSER_CLOSE = False
-X_FRAME_OPTIONS = 'SAMEORIGIN' #For production this needs to be set to DENY
+SESSION_COOKIE_SECURE = boolean(os.environ.get('SESSION_COOKIE_SECURE', 0)) #For production this needs to be set to True
+SESSION_COOKIE_HTTPONLY = boolean(os.environ.get('SESSION_COOKIE_HTTPONLY', 0)) #For production this needs to be set to True
+SESSION_EXPIRE_AT_BROWSER_CLOSE = boolean(os.environ.get('SESSION_EXPIRE_AT_BROWSER_CLOSE', 0))
+X_FRAME_OPTIONS = os.environ.get('X_FRAME_OPTIONS', 'SAMEORIGIN') #For production this needs to be set to DENY
 
 MEDIA_ROOT = os.environ.get('MEDIA_ROOT', local_path('media/'))
 MEDIA_URL = os.environ.get('MEDIA_URL', '/media/')
@@ -60,8 +60,8 @@ LOGOUT_URL = reverse_lazy('logout')
 AUTHENTICATION_BACKENDS = (
     'lily.users.auth_backends.UserModelBackend',
 )
-PASSWORD_RESET_TIMEOUT_DAYS = 7 # Also used as timeout for activation link
-USER_INVITATION_TIMEOUT_DAYS = 7
+PASSWORD_RESET_TIMEOUT_DAYS = os.environ.get('PASSWORD_RESET_TIMEOUT_DAYS', 7) # Also used as timeout for activation link
+USER_INVITATION_TIMEOUT_DAYS = os.environ.get('USER_INVITATION_TIMEOUT_DAYS', 7)
 CUSTOM_USER_MODEL = 'users.UserModel'
 
 STATICFILES_DIRS = (
@@ -147,7 +147,7 @@ TEMPLATED_EMAIL_TEMPLATE_DIR = 'email/'
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'example@provider.com')
 SERVER_EMAIL = os.environ.get('SERVER_EMAIL', 'example@provider.com')
 
-EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', False)
+EMAIL_USE_TLS = boolean(os.environ.get('EMAIL_USE_TLS', 0))
 EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.host.com')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'your-username')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'your-password')
@@ -180,7 +180,7 @@ LOGGING = {
 }
 
 # Cache configuration
-if os.environ.get('REDISTOGO_URL', '') and os.environ.get('ENABLE_CACHE', True):
+if os.environ.get('REDISTOGO_URL', '') and boolean(os.environ.get('ENABLE_CACHE', 1)):
     url = urlparse(os.environ['REDISTOGO_URL'])
     CACHES = {
         'default': {

f6beffb668912a776e833b96550759e77aec1961
snoepkast <allard@solidsquare.nl>
2012-04-12 17:21:18 +0200

    update, project is ready for new heroku push

--- lily/accounts/urls.py
+++ lily/accounts/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 from lily.accounts.views import AddAccountView, EditAccountView, DeleteAccountView, ListAccountView
 
 urlpatterns = patterns('',
-     url(r'^add/?xhr$', AddAccountView.as_view(), name='account_add_xhr'),
+     url(r'^add/\?xhr$', AddAccountView.as_view(), name='account_add_xhr'),
      url(r'^add/$', AddAccountView.as_view(), name='account_add'),
      url(r'^edit/(?P<pk>[\w-]+)/$', EditAccountView.as_view(), name='account_edit'),
      url(r'^delete/xhr/(?P<pk>[\w-]+)/$', DeleteAccountView.as_view(), name='account_delete'),

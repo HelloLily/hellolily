b3402edc1cd2d1b346717207d608c608a86aa103
Cornelis Poppema <cpoppema@gmail.com>
2012-04-16 17:34:00 +0200

    bugfix: display_value templatefilter works after posting data

--- lily/utils/templatetags/field_extras.py
+++ lily/utils/templatetags/field_extras.py
@@ -8,15 +8,18 @@ def field_value(field):
     """ 
     Returns the value for this BoundField, as rendered in widgets. 
     """ 
-    if field.form.is_bound: 
+    if field.form.is_bound:
+        # Return posted value
         if isinstance(field.field, FileField) and field.data is None: 
             val = field.form.initial.get(field.name, field.field.initial) 
         else: 
             val = field.data 
     else:
+        # Return initital value
         val = field.form.initial.get(field.name, field.field.initial)
         if callable(val):
             val = val()
+    # Return empty string if nothing was found rather than None
     if val is None:
         val = ''
     return val
@@ -26,9 +29,18 @@ def display_value(field):
     """ 
     Returns the displayed value for this BoundField, as rendered in widgets. 
     """ 
+    # Find the actual value
     value = field_value(field)
+    # Try making an integer of it (it's unicode on post)
+    try:
+        value = int(value)
+    except:
+        pass
+    # If it's a ChoiceField, try finding the display value
     if isinstance(field.field, ChoiceField): 
         for (val, desc) in field.field.choices: 
             if val == value: 
                 return desc 
     return value
+
+

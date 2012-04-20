05b24f53e523937f7d2671bb9283045c6e5904be
snoepkast <allard@solidsquare.nl>
2012-04-20 16:44:12 +0200

    Fixes todo: add comments to auth backend

--- lily/users/auth_backends.py
+++ lily/users/auth_backends.py
@@ -4,7 +4,15 @@ from django.core.exceptions import ImproperlyConfigured
 from django.db.models import get_model
 
 class UserModelBackend(ModelBackend):
+    """
+    Authenticate the user, instead of using the default User model we use the
+    CustomUser model and return this as well.
+    """
     def authenticate(self, username=None, password=None, no_pass=False):
+        """
+        Check if the user is properly authenticated, either by username
+        and password check (login), or code login (upon activation of account)
+        """
         try:
             user = self.user_class.objects.get(username=username)
             if (user.is_active) and (user.check_password(password) or no_pass):
@@ -13,6 +21,9 @@ class UserModelBackend(ModelBackend):
             return None
 
     def get_user(self, user_id):
+        """
+        Return the custom user model
+        """
         try:
             return self.user_class.objects.get(pk=user_id)
         except self.user_class.DoesNotExist:
@@ -20,6 +31,9 @@ class UserModelBackend(ModelBackend):
 
     @property
     def user_class(self):
+        """
+        Determine which user model to use for authentication etc.
+        """
         if not hasattr(self, '_user_class'):
             self._user_class = get_model(*settings.CUSTOM_USER_MODEL.split('.', 2))            
             if not self._user_class:

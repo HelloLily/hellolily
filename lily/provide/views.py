ea423aa576d48aad0ebab35d910bf7cb00d41716
Allard Stijnman <allard@solidsquare.nl>
2012-05-21 17:04:04 +0200

    New: dataprovider in popup of add account

--- lily/provide/views.py
+++ lily/provide/views.py
@@ -18,60 +18,61 @@ class ProvideBaseView(View):
     api_output = None
     view_output = None
     error_output = None
-    
+
     def get(self, request, *args, **kwargs):
         """
         Save domain from Request or return 404 if the domain is empty.
         """
         # get domain
-        self.domain = kwargs.get('domain').strip()
-        
+        self.kwargs = kwargs
+        self.domain = self.get_domain()
+
         self.api_output = self.do_request_to_api(self.get_url())
-        
+
         try:
             self.view_output = self.set_view_output()
             return self.get_view_output()
         except Exception as e:
             self.set_error_output(e.message)
             return self.get_error_output()
-        
+
     def get_url(self):
         return self.get_api_url() + self.get_query_string()
-    
+
     def get_api_url(self):
         return self.api_url
-    
+
     def get_domain(self):
-        return self.domain
-    
+        return self.domain or self.kwargs.get('domain').strip()
+
     def get_query_kwargs(self):
         return {}
-    
+
     def get_query_string(self):
         kwargs = self.get_query_kwargs()
         if len(kwargs) > 0:
             return '?' + urllib.urlencode(kwargs)
         return ''
-    
+
     def do_request_to_api(self, url):
         """
         Make a request to the given url and return the output.
         """
         response = urllib2.urlopen(url)
         return response.read()
-    
+
     def get_view_output(self):
         return HttpResponse(self.view_output)
-    
+
     def set_view_output(self):
         raise NotImplementedError
-    
+
     def get_error_output(self):
         return HttpResponse(self.error_output)
-    
+
     def set_error_output(self, message):
         self.error_output = simplejson.dumps({'error': {'message': message}})
-    
+
 
 class DataproviderView(ProvideBaseView):
     """
@@ -79,34 +80,35 @@ class DataproviderView(ProvideBaseView):
     """
     api_url = 'http://alpha.dataprovider.com/api/0.1/search.php'
     api_key = settings.DATAPROVIDER_API_KEY
-    
+
     def get_domain(self):
         """
         Return domain without http:// but with 'www.'
         """
+        self.domain = super(DataproviderView, self).get_domain()
+
         if self.domain[:6] == 'http://':
-            self.domain = self.domain[6:]
+                self.domain = self.domain[6:]
         if self.domain[:4] == 'www.':
             self.domain = self.domain[4:]
-            
+
         return self.domain
-    
+
     def get_query_kwargs(self):
         kwargs = {'api_key': self.api_key,
                        'q_field': 'hostname',
-                       'q_value': self.get_domain(),
+                       'q_value': self.domain,
 #                       'q_operator': 5 # filter: ends with
         }
-        print kwargs
         return kwargs
-    
+
     def set_view_output(self, retry=False):
         """
         Create a generic json format for account information based on the json from dataprovider.
         """
         # Expected api output is json
         self.api_output = simplejson.loads(self.api_output)
-        
+
         # Return 404 when nothing was found
         if self.api_output.get('found') == 0:
             if not retry:
@@ -117,15 +119,15 @@ class DataproviderView(ProvideBaseView):
                 self.set_view_output(retry=True)
             else:
                 raise Http404()
-        
+
         # Raise exception when the api returned an error
         if self.api_output.get('error'):
             raise Exception(self.api_output.get('error').get('message'))
-        
+
         # Filter useful data
         result = self.api_output['results'][0]
-        
-        
+
+
         company = result.get('company').replace('\\', '') if result.get('company') else None
         description = result.get('description').replace('\\', '') if result.get('description') else None
         tags = result.get('keywords').strip().rstrip(',').split(',') if result.get('keywords') else []
@@ -137,7 +139,7 @@ class DataproviderView(ProvideBaseView):
         cocnumber = result.get('cocnumber') if result.get('cocnumber') else None
         iban = result.get('iban') if result.get('iban') else None
         bic = result.get('bic') if result.get('bic') else None
-        
+
         address = result.get('address') if result.get('address') else None
         if address:
             street, street_number, complement = parse_address(address)
@@ -168,11 +170,11 @@ class DataproviderView(ProvideBaseView):
             'iban': iban,
             'bic': bic,
         }
-        
+
         return simplejson.dumps(self.view_output)
-    
+
     def get_view_output(self):
         return HttpResponse(self.view_output, mimetype='application/json')
-    
+
     def get_error_output(self):
         raise Http404()

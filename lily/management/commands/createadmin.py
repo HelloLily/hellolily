9614bbeddb66a9af3450ef4d9c5e0ede08aca42b
cpoppema <cpoppema@gmail.com>
2013-10-01 09:52:42 +0200

    Fixed management command 'createadmin'.

--- lily/management/commands/createadmin.py
+++ lily/management/commands/createadmin.py
@@ -7,6 +7,7 @@ from django.utils.encoding import force_unicode
 
 from lily.accounts.models import Account
 from lily.contacts.models import Contact, Function
+from lily.utils.models import EmailAddress
 try:
     from lily.tenant.functions import add_tenant
 except ImportError:
@@ -18,16 +19,16 @@ class Command(NoArgsCommand):
     help = """Create a user which can be used to log in to HelloLily. The default superuser that is prompted \
             for after the syncdb command does not have the appropriate permission groups and is missing \
             the essential relation to a custom model used for log in."""
-            
-    def handle_noargs(self, **options): 
+
+    def handle_noargs(self, **options):
         # First check if there has been created a superuser during syncdb we can use to encapsulate
         # in our own user model.
         superUsers = CustomUser.objects.filter(user_ptr__is_superuser=True)
-        
+
         if len(superUsers) == 0:
             # Create brand new custom user
             self.create_new()
-        elif len(superUsers) == 1:            
+        elif len(superUsers) == 1:
             # Check if a CustomUser is linked to this user
             customUser = CustomUser.objects.filter(user_ptr=superUsers[0])
             if customUser.exists():
@@ -63,7 +64,7 @@ class Command(NoArgsCommand):
                     # Find user by e-mail address
                     user=superUsers.get(contact__email_addresses__email_address__iexact=email)
                     self.create_new(user=user)
-    
+
     def create_new(self, user=None):
         """
         Create an admin with a new account/contact/customuser.
@@ -71,7 +72,7 @@ class Command(NoArgsCommand):
         first_name = self.ask_for_input('Please enter a first name: ')
         last_name = self.ask_for_input('Please enter a last name: ')
         accountname = self.ask_for_input('Please enter an account name: ')
-        
+
         if user is None:
             email_is_unique = False
             email = False
@@ -83,38 +84,39 @@ class Command(NoArgsCommand):
                     email_is_unique = True
                 else:
                     email = raw_input('E-mail address already in use, try again: ').strip()
-            
+
             password1 = 0
             password2 = 1
             while password2 != password1:
                 password1 = getpass.getpass('Your password: ')
                 while password1 is '':
                     password1 = getpass.getpass('Password can\'t be empty: ')
-                    
+
                 password2 = getpass.getpass('Confirm password: ')
                 while password2 is '':
                     password2 = getpass.getpass('Password can\'t be empty: ')
 
                 if password2 != password1:
                     self.stdout.write('Confirmation failed.\n')
-        
+
         contact = Contact(first_name=first_name, last_name=last_name)
         contact, tenant = add_tenant(contact)
         contact.save()
-        
+
         account = Account(name=accountname)
         add_tenant(account, tenant)
         account.save()
-        
+
         function = Function(account=account, contact=contact)
         add_tenant(function, tenant)
         function.save()
-        
+
         if user is None:
             customUser = CustomUser()
             customUser.username = uuid4().get_hex()[:10]
-            customUser.primary_email = email
             customUser.set_password(password2)
+            email_address = EmailAddress.objects.create(is_primary=True, email_address=email, status=1, tenant=tenant)
+            contact.email_addresses.add(email_address)
         else:
             customUser = CustomUser(user_ptr=user)
             # Transfer values from superclass to subclass
@@ -125,12 +127,12 @@ class Command(NoArgsCommand):
         customUser.account = account
         add_tenant(customUser, tenant)
         customUser.save()
-        
+
         if not self.is_user_in_group(customUser):
             self.add_to_group(customUser)
-        
+
         self.stdout.write('Admin created.\n')
-    
+
     def add_to_group(self, user, group_name='account_admin'):
         """
         Add a user to given group.
@@ -138,14 +140,14 @@ class Command(NoArgsCommand):
         # Get group
         group, created = Group.objects.get_or_create(name='account_admin')
         user.groups.add(group)
-    
+
     def is_user_in_group(self, user, group_name='account_admin'):
         """
         Return True if a user is in a certain group, False otherwise.
         """
         group = Group.objects.filter(name=group_name)
-        return group in user.groups.all()            
-        
+        return group in user.groups.all()
+
     def ask_for_input(self, question):
         user_input = raw_input(question).strip()
         while user_input is '':
@@ -153,7 +155,7 @@ class Command(NoArgsCommand):
                 self.stdout.write('Input can\'t be empty: ')
             user_input = raw_input('').strip()
         return user_input
-    
+
     def ask_for_yes_no_input(self, question):
         user_input = self.ask_for_input(question).strip()
         while user_input not in ['yes', 'y', 'no', 'n']:
@@ -161,4 +163,4 @@ class Command(NoArgsCommand):
                 self.stdout.write('Please answer yes or no: ')
             user_input = raw_input('').strip()
         return (user_input[:1] == 'y')
-        
+

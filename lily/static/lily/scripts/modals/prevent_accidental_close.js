8fb9d677cc9949bb473ec82705d159c15ddd4230
Ednan <ednanpasagic@gmail.com>
2014-06-05 11:23:32 +0200

    LILY-311: Made sure confirm close popup isn't displayed all the time

--- lily/static/lily/scripts/modals/prevent_accidental_close.js
+++ lily/static/lily/scripts/modals/prevent_accidental_close.js
@@ -4,9 +4,12 @@ $(function() {
     });
 
     $('body').on('hide.bs.modal', '.modal', function (e) {
-        if ($(this).find('form').data('serialized_form') != $(this).find('form').serialize()) {
-            if (!confirm('Are you sure?')) {
-                e.preventDefault();
+        // this check is to make sure not every widget with a popup displays the confirm message
+        if ($(e.target).hasClass('modal')) {
+            if ($(this).find('form').data('serialized_form') != $(this).find('form').serialize()) {
+                if (!confirm('Are you sure?')) {
+                    e.preventDefault();
+                }
             }
         }
     });

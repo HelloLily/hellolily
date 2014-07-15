d49a96a21d2c19a52f00fd4f256aa79bba730722
Ednan <ednanpasagic@gmail.com>
2014-07-15 14:58:20 +0200

    Added HostnameValidator

---
+++ lily/utils/validators.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, url
+
+from lily.utils.views import ajax_update_view, notifications_view
+
+
+urlpatterns = patterns('',
+                       url(r'^ajax/(?P<app_name>[A-Za-z]+)/(?P<model_name>[A-Za-z]+)/(?P<object_id>[0-9]+)/$', ajax_update_view, name='ajax_update_view'),
+                       url(r'^utils/notifications.js$', notifications_view, name='notifications'),
+                       )

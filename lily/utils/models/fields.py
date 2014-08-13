353a5bda7cf310cd427dd81100328b66c8524652
Bob Voorneveld <bob.voorneveld@voipgrid.nl>
2014-08-13 14:56:50 +0200

    Fixed migration email and south migration issue

--- lily/utils/models/fields.py
+++ lily/utils/models/fields.py
@@ -1,4 +1,5 @@
 from django.db import models
+from south.modelsinspector import add_introspection_rules
 
 from ..forms.fields import FormSetField
 from ..forms.formsets import PhoneNumberFormSet, AddressFormSet, EmailAddressFormSet
@@ -28,13 +29,16 @@ class BaseFormSetField(models.ManyToManyField):
 class PhoneNumberFormSetField(BaseFormSetField):
     template = 'utils/formset_phone_number.html'
     formset_class = PhoneNumberFormSet
+add_introspection_rules([], ["^lily\.utils\.models\.fields\.PhoneNumberFormSetField"])
 
 
 class AddressFormSetField(BaseFormSetField):
     template = 'utils/formset_address.html'
     formset_class = AddressFormSet
+add_introspection_rules([], ["^lily\.utils\.models\.fields\.AddressFormSetField"])
 
 
 class EmailAddressFormSetField(BaseFormSetField):
     template = 'utils/formset_email_address.html'
     formset_class = EmailAddressFormSet
+add_introspection_rules([], ["^lily\.utils\.models\.fields\.EmailAddressFormSetField"])

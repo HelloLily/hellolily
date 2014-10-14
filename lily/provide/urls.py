1d355186d2b40bbb4b23ef6c64990d8ad3387701
Bob Voorneveld <bob.voorneveld@voipgrid.nl>
2014-10-14 12:54:09 +0200

    LILY-417: make dataprovider accept urls with -

--- lily/provide/urls.py
+++ lily/provide/urls.py
@@ -4,5 +4,5 @@ from lily.provide.views import DataproviderView
 
 
 urlpatterns = patterns('',
-    url(r'^account/(?P<domain>[\.0-9a-zA-Z]+)/$', DataproviderView.as_view(), name='provide_account'),
+    url(r'^account/(?P<domain>[\.0-9a-zA-Z\-]+)/$', DataproviderView.as_view(), name='provide_account'),
 )

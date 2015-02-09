824a240abbe553886225d7e21abb91b7f2b27f57
Ednan <ednanpasagic@gmail.com>
2015-02-09 16:51:36 +0100

    LILY-587: Add assign to me to case detail

--- lily/cases/views.py
+++ lily/cases/views.py
@@ -12,6 +12,8 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from lily.accounts.models import Account
 from lily.contacts.models import Contact
 from lily.notes.models import Note
+from lily.tenant.middleware import get_current_user
+from lily.users.models import LilyUser
 from lily.utils.functions import is_ajax
 from lily.utils.views import AjaxUpdateView, ArchiveView, UnarchiveView, AngularView
 from lily.utils.views.mixins import HistoryListViewMixin, LoginRequiredMixin
@@ -280,7 +282,7 @@ class DeleteCaseView(LoginRequiredMixin, DeleteView):
 
 class UpdateStatusAjaxView(AjaxUpdateView):
     """
-    View that updates the status-field of a Case.
+    View that updates the status field of a Case.
     """
     http_method_names = ['post']
 
@@ -311,6 +313,60 @@ class UpdateStatusAjaxView(AjaxUpdateView):
         messages.success(self.request, message)
         # Return response
         return HttpResponse(json.dumps({'status': status.status}), content_type='application/json')
+
+
+class UpdateAssignedToView(AjaxUpdateView):
+    """
+    View that updates the assigned to field of a Case.
+    """
+    http_method_names = ['post']
+
+    def post(self, request, *args, **kwargs):
+        """
+        Overloading post to update the assigned_to attribute for a Case object.
+        """
+        assignee = None
+
+        try:
+            assignee_id = request.POST.get('assignee', None)
+
+            if assignee_id:
+                # FIXME: WORKAROUND FOR TENANT FILTER.
+                # An error will occur when using LilyUser.objects.all(), most likely because
+                # the foreign key to contact (and maybe account) is filtered and executed before
+                # the filter for the LilyUser. This way it's possible contacts (and maybe accounts)
+                # won't be found for a user. But since it's a required field, an exception is raised.
+                assignee = LilyUser.objects.get(pk=int(assignee_id), tenant=get_current_user().tenant.id)
+        except LilyUser.DoesNotExist:
+            messages.error(self.request, _('Assignee could not be changed'))
+            raise Http404()
+
+        try:
+            case = Case.objects.get(pk=kwargs.pop('pk'))
+        except Case.DoesNotExist:
+            messages.error(self.request, _('Assignee could not be changed'))
+            raise Http404()
+
+        case.assigned_to = assignee
+        case.save()
+
+        if assignee:
+            message = _('Assignee has been changed to %s') % assignee.full_name
+            messages.success(self.request, message)
+            # Return response
+            return HttpResponse(anyjson.serialize({
+                'assignee': {
+                    'id': assignee.id,
+                    'name': assignee.full_name,
+                }
+            }), content_type='application/json')
+        else:
+            message = _('Case has been unassigned')
+            messages.success(self.request, message)
+            # Return response
+            return HttpResponse(anyjson.serialize({
+                'assignee': None
+            }), content_type='application/json')
 
 
 class GetCaseTypesView(LoginRequiredMixin, View):

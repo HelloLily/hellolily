8d1eef94cc345277d639a9bbdc37ccc37cc4b45a
Bob Voorneveld <bob.voorneveld@voipgrid.nl>
2015-02-23 09:47:12 +0100

    Cleanup after django 1.7 migration

--- lily/google/models.py
+++ lily/google/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from oauth2client.django_orm import FlowField
-from south.modelsinspector import add_introspection_rules
 
 from lily.users.models import LilyUser
 
@@ -12,7 +11,3 @@ class FlowModel(models.Model):
     """
     id = models.ForeignKey(LilyUser, primary_key=True)
     flow = FlowField()
-
-
-add_introspection_rules([], ["^oauth2client\.django_orm\.FlowField"])
-add_introspection_rules([], ["^oauth2client\.django_orm\.CredentialsField"])

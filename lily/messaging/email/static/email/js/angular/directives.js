d7d50e3069517d05fa9124723a07bd31ac44c577
Bob Voorneveld <bob.voorneveld@voipgrid.nl>
2015-05-18 16:14:16 +0200

    LILY-892: fixed unneeded search on undefined emailaddress

--- lily/messaging/email/static/email/js/angular/directives.js
+++ lily/messaging/email/static/email/js/angular/directives.js
@@ -1,22 +1,25 @@
-/**
- * EmailDirectives is a container for all global lily related Angular directives
- */
-var EmailDirectives = angular.module('EmailDirectives', []);
+(function () {
+    'use strict';
 
-/**
- * contactIcon Directive shows how the email is connected with an account or contact
- *
- * @param message object: object with message info
- *
- * Example:
- *
- * <td contact-icon message="message"></td>
- *
- */
-EmailDirectives.directive('contactIcon', [
-    '$http',
-    '$modal',
-    function($http, $modal) {
+    /**
+     * app.email.directives is a container for all global lily related Angular directives
+     */
+    angular.module('app.email.directives', []);
+
+    /**
+     * contactIcon Directive shows how the email is connected with an account or contact
+     *
+     * @param message object: object with message info
+     *
+     * Example:
+     *
+     * <td contact-icon message="message"></td>
+     *
+     */
+    angular.module('app.email.directives').directive('contactIcon', contactIcon);
+
+    contactIcon.$inject = ['$http'];
+    function contactIcon ($http) {
         return {
             restrict: 'A',
             scope: {
@@ -24,99 +27,36 @@ EmailDirectives.directive('contactIcon', [
             },
             replace: true,
             templateUrl: 'email/directives/contact-icon.html',
-            link: function(scope, element, attrs) {
+            link: function (scope, element, attrs) {
 
                 // Do we have an associated account or contact?
-                $http.get('/search/emailaddress/' + scope.message.sender_email)
-                    .success(function(data) {
-                        scope.emailAddressResults = data;
-                        if (data.type == 'contact') {
-                            // Contact and has account
-                            if (data.data.account) {
-                                scope.status = 'complete';
-                            // Contact without account
+                if (scope.message.sender_email) {
+                    $http.get('/search/emailaddress/' + scope.message.sender_email)
+                        .success(function (data) {
+                            scope.emailAddressResults = data;
+                            if (data.type == 'contact') {
+                                // Contact and has account
+                                if (data.data.account) {
+                                    scope.status = 'complete';
+                                    // Contact without account
+                                } else {
+                                    scope.status = 'needsAccount';
+                                }
+                            } else if (data.type == 'account') {
+                                // Is the emailadress from the account it self (eg. info@)
+                                if (data.complete) {
+                                    scope.status = 'complete';
+                                } else {
+                                    scope.status = 'needsContact';
+                                }
                             } else {
-                                scope.status = 'needsAccount';
+                                scope.status = 'needsEverything';
                             }
-                        }else if (data.type == 'account') {
-                            // Is the emailadress from the account it self (eg. info@)
-                            if (data.complete == true) {
-                                scope.status = 'complete';
-                            } else {
-                                scope.status = 'needsContact';
-                            }
-                        } else {
-                            scope.status = 'needsEverything';
-                        }
-                    });
-
-                // TODO: Create modal
-                //scope.openModal = function() {
-                //    $modal.open({
-                //        templateUrl: '/static/lily/angular/email/templates/contact-modal.html',
-                //        controller: 'ContactModalController',
-                //        size: 'lg',
-                //        resolve: {
-                //            action: function() {
-                //                return scope.action;
-                //            },
-                //            emailAddressResults: function () {
-                //                return scope.emailAddressResults;
-                //            },
-                //            message: function() {
-                //                return scope.message;
-                //            }
-                //        }
-                //    });
-                //}
+                        });
+                } else {
+                    scope.status = 'complete';
+                }
             }
         };
-    }]
-);
-
-// TODO: create modal
-//
-//
-//EmailDirectives.controller('ContactModalController', [
-//    '$scope',
-//    '$modalInstance',
-//    '$http',
-//
-//    'action',
-//    'emailAddressResults',
-//    'message',
-//    function($scope, $modalInstance, $http, action, emailAddressResults, message) {
-//
-//        $scope.account = {};
-//
-//        if (action == 'needsEverything') {
-//            $scope.account.url = 'http://www.' + message.sender_email.split('@')[1] + '/';
-//        }
-//
-//        $scope.fetchDomain = function() {
-//            console.log($scope.account.url);
-//
-//            if ($scope.account.url) {
-//                var domain = $scope.account.url.replace('http://', '').replace('https://');
-//            // Always add last '/'
-//            if (domain.slice(-1) !== '/') {
-//                domain += '/';
-//            }
-//
-//                $http.get('/provide/account/' + domain)
-//                    .success(function(data) {
-//                        console.log(data);
-//                    });
-//            }
-//
-//        };
-//
-//        $scope.ok = function () {
-//            $modalInstance.close();
-//        };
-//
-//        $scope.cancel = function () {
-//            $modalInstance.dismiss();
-//        };
-//    }]
-//);
+    }
+})();

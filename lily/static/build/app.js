a92c49479b531c81486a53194fd871ca30b3a977
Bob Voorneveld <bob.voorneveld@voipgrid.nl>
2015-06-10 15:10:24 +0200

    LILY-963: fixed save and archive

--- lily/static/build/app.js
+++ lily/static/build/app.js
@@ -1851,56 +1851,54 @@ function addBusinessDays(date, businessDays) {
 })(angular);
 (function(angular){
 'use strict';
-$(function() {
+var $body = $('body');
+$body.on('blur', 'input[name^="phone"]', function() {
+    // Format telephone number
+    var $phoneNumberInput = $(this);
+    var phone = $phoneNumberInput.val();
+    if (phone.match(/[a-z]|[A-Z]/)) {
+        // if letters are found, skip formatting: it may not be a phone field after all
+        return false;
+    }
 
-    $('body').on('blur', 'input[name^="phone"]', function() {
-        // Format telephone number
-        var $phoneNumberInput = $(this);
-        var phone = $phoneNumberInput.val();
-        if (phone.match(/[a-z]|[A-Z]/)) {
-            // if letters are found, skip formatting: it may not be a phone field after all
-            return false;
+    // Match on mobile phone nrs e.g. +316 or 06, so we can automatically set the type to mobile.
+    if (phone.match(/^\+316|^06/)) {
+        var typeId = $phoneNumberInput.attr('id').replace('raw_input', 'type');
+        $('#' + typeId).select2('val', 'mobile');
+    }
+
+    phone = phone
+        .replace("(0)","")
+        .replace(/[\s\(\-\)\.\\\/\–x:\*]/g, "")
+        .replace(/^00/,"+");
+
+    if (phone.length == 0) {
+        return false;
+    }
+
+    if (!phone.startsWith('+')) {
+        if (phone.startsWith('0')) {
+            phone = phone.substring(1);
         }
+        phone = '+31' + phone;
+    }
 
-        // Match on mobile phone nrs e.g. +316 or 06, so we can automatically set the type to mobile.
-        if (phone.match(/^\+316|^06/)) {
-            var typeId = $phoneNumberInput.attr('id').replace('raw_input', 'type');
-            $('#' + typeId).select2('val', 'mobile');
-        }
+    if (phone.startsWith('+310')) {
+        phone = '+31' + phone.substring(4);
+    }
+    $phoneNumberInput.val(phone);
+});
 
-        phone = phone
-            .replace("(0)","")
-            .replace(/[\s\(\-\)\.\\\/\–x:\*]/g, "")
-            .replace(/^00/,"+");
-
-        if (phone.length == 0) {
-            return false;
-        }
-
-        if (!phone.startsWith('+')) {
-            if (phone.startsWith('0')) {
-                phone = phone.substring(1);
+$body.on('change', 'select[id*="is_primary"]', function(e) {
+    if($(e.currentTarget).val() == 'True'){
+        $('select[id*="is_primary"]').each(function(i){
+            if($(this).is('select') && $(this).val() == 'True'){
+                $(this).val('False');
             }
-            phone = '+31' + phone;
-        }
-
-        if (phone.startsWith('+310')) {
-            phone = '+31' + phone.substring(4);
-        }
-        $phoneNumberInput.val(phone);
-    });
-
-    $('body').on('change', 'select[id*="is_primary"]', function(e) {
-        if($(e.currentTarget).val() == 'True'){
-            $('select[id*="is_primary"]').each(function(i){
-                if($(this).is('select') && $(this).val() == 'True'){
-                    $(this).val('False');
-                }
-            });
-            $(e.currentTarget).val('True');
-            HLSelect2.init();
-        }
-    });
+        });
+        $(e.currentTarget).val('True');
+        HLSelect2.init();
+    }
 });
 
 function addBusinessDays(date, businessDays) {
@@ -2599,6 +2597,40 @@ function resizeIframe () {
                         ifRef.height = ifDoc.scrollHeight;
                     }
                 }
+            });
+        }
+    }
+}
+
+})(angular);
+(function(angular){
+'use strict';
+angular.module('app.directives').directive('saveAndArchive', saveAndArchiveDirective);
+
+function saveAndArchiveDirective () {
+    return {
+        restrict: "A",
+        link: function(scope, elem, attrs) {
+
+            // Setting button to right text based in archived state
+            var $button = $('#archive-button');
+            var $archiveField = $('#id_is_archived');
+            if ($archiveField.val() === 'True') {
+                $button.find('span').text('Save and Unarchive');
+            } else {
+                $button.find('span').text('Save and Archive');
+            }
+
+            // On button click set archived hidden field and submit form
+            elem.on('click', function () {
+                $button = $('#archive-button');
+                $archiveField = $('#id_is_archived');
+                var $form = $($button.closest('form').get(0));
+                var archive = ($archiveField.val() === 'True' ? 'False' : 'True');
+                $archiveField.val(archive);
+                $button.button('loading');
+                $form.find(':submit').click();
+                event.preventDefault();
             });
         }
     }

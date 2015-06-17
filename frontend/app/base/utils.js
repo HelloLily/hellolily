4e57817ebac8c7dc30e542268595ab25f624382b
Bob Voorneveld <bob.voorneveld@voipgrid.nl>
2015-06-17 14:56:40 +0200

    LILY-965: assignee & contact/account required for case

--- frontend/app/base/utils.js
+++ frontend/app/base/utils.js
@@ -46,19 +46,3 @@ $('body').on('change', 'select[id*="is_primary"]', function(e) {
         HLSelect2.init();
     }
 });
-
-function addBusinessDays(date, businessDays) {
-    var weeks = Math.floor(businessDays/5);
-    var days = businessDays % 5;
-    var day = date.getDay();
-    if (day === 6 && days > -1) {
-       if (days === 0) {days-=2; day+=2;}
-       days++; dy -= 6;}
-    if (day === 0 && days < 1) {
-       if (days === 0) {days+=2; day-=2;}
-       days--; day += 6;}
-    if (day + days > 5) days += 2;
-    if (day + days < 1) days -= 2;
-    date.setDate(date.getDate() + weeks * 7 + days);
-    return date;
-}
--- frontend/app/cases/controllers/create.js
+++ frontend/app/cases/controllers/create.js
@@ -50,6 +50,52 @@ CaseCreateController.$inject = ['$scope'];
 function CaseCreateController ($scope) {
     $scope.conf.pageTitleBig = 'New case';
     $scope.conf.pageTitleSmall = 'making cases';
-    HLCases.addAssignToMeButton();
-    HLSelect2.init();
+
+    activate();
+
+    ////
+
+    function activate () {
+        HLCases.addAssignToMeButton();
+        HLSelect2.init();
+        _setupSubmitListener();
+    }
+
+    function _setupSubmitListener () {
+        // We need to check if the form has on assignee.
+        var $assigned_to = angular.element('#id_assigned_to');
+        var $form = $($assigned_to.closest('form')[0]);
+        $form.on('submit', function(event) {
+
+            // Check account or contact
+            if ((angular.element('#id_account').val() == '' || angular.element('#id_account').val() == -1) &&
+                (angular.element('#id_contact').val() == '' || angular.element('#id_contact').val() == -1)) {
+                event.preventDefault();
+                bootbox.dialog({
+                    message: 'Please select an account or contact the case belongs to',
+                    title: 'No account or contact',
+                    buttons: {
+                        success: {
+                            label: 'Let me fix that for you',
+                            className: 'btn-success'
+                        }
+                    }
+                });
+            // Check if case is assigned
+            } else if ($assigned_to.val() == '' && angular.element('#id_assigned_to_groups').val() == null) {
+                // No assignee, let's cancel the submit & show alert to user.
+                event.preventDefault();
+                bootbox.dialog({
+                    message: 'Please select a colleague or team to assign the case to',
+                    title: 'No assignee set',
+                    buttons: {
+                        success: {
+                            label: 'Let me fix that for you',
+                            className: 'btn-success'
+                        }
+                    }
+                });
+            }
+        });
+    }
 }

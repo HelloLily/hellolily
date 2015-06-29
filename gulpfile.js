255dee8ecc8a641195076a8cbd70e04b8528638b
Allard Stijnman <a.g.stijnman@gmail.com>
2015-06-29 15:14:31 +0200

    Final touches to gulp and the pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,27 @@
 'use strict';
 
 // Imports
+var cached = require('gulp-cached');  // Only work on changed files
 var cdnizer = require('gulp-cdnizer'); // Prepend CDN url to resources
 var cleanhtml = require('gulp-cleanhtml');  // strip whitespace eg.
 var concat = require('gulp-concat');  // merge file stream to one file
 var del = require('del');  // remove files/dirs
 var gulp = require('gulp');  // base module
 var fs = require('fs');  // File system
+var imagemin = require('gulp-imagemin');  // Minify/optimize images
 var livereload = require('gulp-livereload');  // live reload server (needs plugin install in Chrome)
 var merge = require('merge-stream');  // Merge multiple gulp streams together
 var path = require('path');  // File path helpers
+var rebaseUrls = require('gulp-css-rebase-urls');  // Make relative paths absolute
+var remember = require('gulp-remember');  // Remember all files after a cached call
 var rename = require('gulp-rename');  // rename current file stream
-var runSequence = require('run-sequence'); // Run tasks in sequence
 var sass = require('gulp-sass');  // Sass compilation
 var sourcemaps = require('gulp-sourcemaps');  // create sourcemaps from original files and create a .map file
 var templateCache = require('gulp-angular-templatecache');  // create out of html files Angular templates in one js file/stream
 var uglify = require('gulp-uglify');  // minify javascript file
 var uglifyCss = require('gulp-uglifycss');  // minify css file
-var watch = require('gulp-watch');
+var watch = require('gulp-watch');  // Optimized file change watcher
 var wrap = require('gulp-wrap');  // surround current file(s) with other content (IIFE eg.)
-
 
 /**
  * Config for Gulp
@@ -28,21 +30,22 @@ var config = {
     app: {
         buildDir: 'lily/static/',
         js: {
-            modules: {
-                basepath: 'frontend/app/',
-                buildDir: 'lily/static/modules/',
-                src: [
-                    '**/module.js',
-                    '**/*.js'
-                ]
-            },
             src: [
-                'lily/static/modules/*.js'  // Should match build dir of modules
+                'frontend/app/**/module.js',
+                'frontend/app/**/*.js',
+                '!frontend/app/base/analytics.js'
             ],
-            fileName: 'app.js'
+            fileName: 'app.js',
+            analytics: {
+                src: [
+                    'frontend/app/base/analytics.js'
+                ],
+                fileName: 'analytics.js'
+            }
         },
         sass: {
-            src: 'frontend/app/app.scss',
+            src: ['frontend/app/**/*.scss'],
+            base: 'frontend/app/app.scss',
             fileName: 'app.css'
         },
         templates: {
@@ -68,9 +71,16 @@ var config = {
             src: [
                 'frontend/vendor/**/*jquery.min.js',
                 'frontend/vendor/**/*angular.js',
-                'frontend/vendor/**/*.js'
+                'frontend/vendor/**/*.js',
+                '!frontend/vendor/metronic/assets/global/plugins/ie-fixes/**/*.js'  // IE fixes
             ],
-            fileName: 'vendor.js'
+            fileName: 'vendor.js',
+            IEFixes: {
+                src: [
+                    'frontend/vendor/metronic/assets/global/plugins/ie-fixes/**/*.js'
+                ],
+                fileName: 'ie-fixes.js'
+            }
         },
         css: {
             src: [
@@ -92,50 +102,21 @@ var config = {
         }
     },
     cdn: {
-        defaultBase: "/static/",  // static for local cloudfront url for live.
-        src: {
-            imgs: '**/*.{gif,png,jpg,jpeg,svg,ico}',  // Images/graphics
-            fonts: '**/*.{eot,otf,ttf,woff,woff2}',  // Fonts
-            files: '**/*.{css,js}'  // Files
+        defaultBase: process.env.STATIC_URL,  // static url to prepend to all file paths.
+        root: 'frontend/',
+        src: [
+            '**/*.{gif,png,jpg,jpeg,svg,ico}',  // Images
+            '**/*.{eot,otf,ttf,woff,woff2}',  // Fonts with all types of crud on the back
+            '**/*.{eot,otf,ttf,woff,woff2}?*',
+            '**/*.{eot,otf,ttf,woff,woff2}#*',
+            '**/*.{css,js}'  // Files
+        ],
+        images: {
+            optimizationLevel: 3,
+            multipass: true
         }
     }
 };
-
-/**
- * Helper functions for gulp
- */
-function getFolders(dir) {
-    return fs.readdirSync(dir).filter(function(file) {
-        return fs.statSync(path.join(dir, file)).isDirectory();
-    });
-}
-
-function getJsModuleSrc(folder) {
-    var moduleSrc = [];
-    folder = (folder !== "undefined") ? folder : '';
-    for (var i in config.app.js.modules.src) {
-        moduleSrc.push(path.join(config.app.js.modules.basepath, folder, config.app.js.modules.src[i]));
-    }
-    return moduleSrc;
-}
-
-function buildJsModule(folder) {
-    // make array with filepaths (module.js first)
-    var moduleSrc = getJsModuleSrc(folder);
-
-    return gulp.src(moduleSrc)
-        // Create source map
-        .pipe(sourcemaps.init())
-        // Wrap in IIFE
-        .pipe(wrap('(function(angular){\n\'use strict\';\n<%= contents %>\n})(angular);'))
-        // Concatenate into foldername.min.js
-        .pipe(concat(folder + '.js'))
-        // Minify
-        .pipe(uglify())
-        // Write to output (both the source and sourcemap)
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(config.app.js.modules.buildDir));
-}
 
 /**
  * Clean build dir
@@ -146,25 +127,15 @@ gulp.task('clean', [], function() {
     ]);
 });
 
-/**
- * Concatenate and minify all app js per module (folder)
- */
-gulp.task('app-js-modules', [], function() {
-    var folders = getFolders(config.app.js.modules.basepath);
-
-    var modules = folders.map(function(folder) {
-        return buildJsModule(folder);
-    });
-
-    return merge(modules);
-});
-
-/**
- * Concatenate all modules into a single app js
- */
 gulp.task('app-js', [], function() {
     return gulp.src(config.app.js.src)
-        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(sourcemaps.init())
+
+        .pipe(cached('app-js'))
+        .pipe(wrap('(function(angular){\'use strict\';<%= contents %>})(angular);'))
+        .pipe(uglify())
+        .pipe(remember('app-js'))
+
         .pipe(concat(config.app.js.fileName))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(config.app.buildDir))
@@ -172,14 +143,12 @@ gulp.task('app-js', [], function() {
 });
 
 gulp.task('app-css', [], function() {
-    return gulp.src(config.app.sass.src)
+    return gulp.src(config.app.sass.base)
         .pipe(sass().on('error', sass.logError))
+        .pipe(rebaseUrls({root: config.cdn.root}))
         .pipe(cdnizer({
             defaultCDNBase: config.cdn.defaultBase,
-            files: [
-                config.cdn.src.imgs,
-                config.cdn.src.fonts
-            ]
+            files: config.cdn.src
         }))
         .pipe(sourcemaps.init({loadMaps: true}))
         .pipe(uglifyCss())
@@ -196,14 +165,11 @@ gulp.task('app-templates', [], function() {
     return gulp.src(config.app.templates.src)
         .pipe(cdnizer({
             defaultCDNBase: config.cdn.defaultBase,
-            files: [
-                config.cdn.src.imgs,
-                config.cdn.src.files
-            ]
+            files: config.cdn.src
         }))
         .pipe(cleanhtml())
         .pipe(templateCache(config.app.templates.fileName, {module: config.app.templates.module, standalone:true}))
-        .pipe(wrap('(function(angular){\n\'use strict\';\n<%= contents %>\n})(angular);'))
+        .pipe(wrap('(function(angular){\'use strict\';<%= contents %>})(angular);'))
         .pipe(uglify())
         .pipe(gulp.dest(config.app.buildDir))
         .pipe(livereload());
@@ -211,6 +177,10 @@ gulp.task('app-templates', [], function() {
 
 gulp.task('app-assets', [], function(){
     return gulp.src(config.app.assets.src)
+        .pipe(imagemin({
+            optimizationLevel: config.cdn.images.optimizationLevel,
+            multipass: config.cdn.images.multipass
+        }))
         .pipe(gulp.dest(config.app.buildDir))
         .pipe(livereload());
 });
@@ -221,7 +191,22 @@ gulp.task('app-assets', [], function(){
 gulp.task('vendor-js', [], function() {
     return gulp.src(config.vendor.js.src)
         .pipe(sourcemaps.init())
+
+        .pipe(cached('vendor-js'))
+        .pipe(uglify())
+        .pipe(remember('vendor-js'))
+
         .pipe(concat(config.vendor.js.fileName))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(config.vendor.buildDir))
+        .pipe(livereload());
+});
+
+gulp.task('ie-fixes', [], function(){
+    return gulp.src(config.vendor.js.IEFixes.src)
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(concat(config.vendor.js.IEFixes.fileName))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(config.vendor.buildDir))
         .pipe(livereload());
@@ -230,17 +215,17 @@ gulp.task('vendor-js', [], function() {
 gulp.task('vendor-css', [], function() {
     return gulp.src(config.vendor.css.src)
         .pipe(sourcemaps.init())
+
+        .pipe(cached('vendor-css'))
+        .pipe(rebaseUrls({root: config.cdn.root}))
         .pipe(cdnizer({
             defaultCDNBase: config.cdn.defaultBase,
-            files: [
-                config.cdn.src.fonts,
-                config.cdn.src.imgs,
-                config.cdn.src.files
-            ],
-            relativeRoot: 'vendor/'
+            files: config.cdn.src
         }))
-        .pipe(concat(config.vendor.css.fileName))
         .pipe(uglifyCss())
+        .pipe(remember('vendor-css'))
+
+        .pipe(concat(config.vendor.css.fileName))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(config.vendor.buildDir))
         .pipe(livereload());
@@ -248,49 +233,39 @@ gulp.task('vendor-css', [], function() {
 
 gulp.task('vendor-assets', [], function() {
     return gulp.src(config.vendor.assets.src)
+        .pipe(imagemin({
+            optimizationLevel: config.cdn.images.optimizationLevel,
+            multipass: config.cdn.images.multipass
+        }))
         .pipe(gulp.dest(config.vendor.buildDir))
         .pipe(livereload());
+});
+
+gulp.task('analytics', [], function() {
+    // TODO: please remove this in the future
+    return gulp.src(config.app.js.analytics.src)
+        .pipe(rename(config.app.js.analytics.fileName))
+        .pipe(gulp.dest(config.app.buildDir));
 });
 
 /**
  * Concatenate, minify and make source maps of all js and css
  */
-gulp.task('build', [], function() {
-    runSequence(
-        'app-js-modules',  // Because of app-js this needs to run first
-        ['app-js', 'app-css', 'app-templates', 'app-assets', 'vendor-js', 'vendor-css', 'vendor-assets']
-    );
-});
+gulp.task('build', ['app-js', 'app-css', 'app-templates', 'app-assets', 'vendor-js', 'vendor-css', 'vendor-assets', 'analytics', 'ie-fixes'], function() {});
 
 /**
  * Watch for changes
  */
 gulp.task('watch', [], function() {
-    // Watch for js module changes
-    watch(config.app.js.modules.basepath + '**/*.js', function(vinyl) {
-        var modules = vinyl.history.map(function(path) {
-            var basepath = config.app.js.modules.basepath;
+    livereload.listen();
 
-            var folder = path.slice(path.search(basepath) + basepath.length);
-            folder = folder.slice(0, folder.search('/'));
-
-            console.log('Recompiling ' + folder + '.js');
-
-            return buildJsModule(folder)
-        });
-
-        console.log('Done recompiling');
-
-        return merge(modules)
-    });
-
-    // Watch for js changes in any of the modules
-    watch(config.app.js.modules.buildDir + '**/*.js', function() {
+    // Watch for changes in app javascript
+    watch(config.app.js.src, function() {
         gulp.start('app-js');
     });
 
     // Watch for changes in sass files
-    watch('frontend/app/**/*.scss', function() {
+    watch(config.app.sass.src, function() {
         gulp.start('app-css')
     });
 
@@ -302,6 +277,11 @@ gulp.task('watch', [], function() {
     // Gather all other static from the app
     watch(config.app.assets.src, function() {
         gulp.start('app-assets');
+    });
+
+    // Watch google analytics script
+    watch(config.app.js.analytics.src, function() {
+        gulp.start('analytics');
     });
 
     // Recompile all vendor js
@@ -318,11 +298,16 @@ gulp.task('watch', [], function() {
     watch(config.vendor.assets.src, function(){
         gulp.start('vendor-assets');
     });
+
+    // JS with IE fixes
+    watch(config.vendor.js.IEFixes.src, function(){
+        gulp.start('ie-fixes');
+    });
 });
 
 /**
  * Set the default command to run
  */
 gulp.task('default', ['build'], function() {
-
+    gulp.start('watch');
 });
--- lily/pipeline/bundles.py
+++ lily/pipeline/bundles.py
@@ -1,44 +1,10 @@
-
-PIPELINE_CSS = {
-    'anonymous': {
-        'source_filenames': (
-            'metronic/assets/global/plugins/font-awesome/css/font-awesome.css',
-            'metronic/assets/global/plugins/simple-line-icons/simple-line-icons.css',
-            'metronic/assets/global/plugins/bootstrap/css/bootstrap.css',
-            'metronic/assets/global/plugins/uniform/css/uniform.default.css',
-            'metronic/assets/global/plugins/bootstrap-toastr/toastr.css',
-            'metronic/assets/global/css/components-rounded.css',
-            'metronic/assets/global/css/plugins.css',
-            'metronic/assets/admin/layout4/css/layout.css',
-            'metronic/assets/admin/layout4/css/themes/light.css',
-            'users/css/custom.css',
-        ),
-        'output_filename': 'metronic/css/anonymous_global.css',
-    },
-    'vendor': {
-        'source_filenames': (
-            'build/vendor.css',
-        ),
-        'output_filename': 'compiled/css/vendor.css',
-    },
-    'app': {
-        'source_filenames': (
-            'build/app.css',
-        ),
-        'output_filename': 'compiled/css/app.css',
-    },
-}
-
 PIPELINE_JS = {
     'anonymous': {
         'source_filenames': (
-            'metronic/assets/global/plugins/jquery.min.js',
-            'metronic/assets/global/plugins/jquery-migrate.min.js',
-            'metronic/assets/global/plugins/jquery-ui/jquery-ui.min.js',
+            'vendor/vendor.js',
 
-            'metronic/assets/global/plugins/bootstrap/js/bootstrap.min.js',
-            'metronic/assets/global/plugins/bootstrap-toastr/toastr.js',
-            'lily/plugins/analytics/analytics.js',
+            # Lily static
+            'analytics.js',
 
             # Pip package static
             'django_password_strength/js/zxcvbn.js',
@@ -59,7 +25,7 @@ PIPELINE_JS = {
             'users/js/angular/filters.js',
 
             # Pip package static
-            'js/jquery.formset.js',
+            # 'js/jquery.formset.js',
             'django_password_strength/js/zxcvbn.js',
             'django_password_strength/js/password_strength.js',
         ),

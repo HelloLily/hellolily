0c4b301e1b2a0ecccc7ed2e5e0cb10060c42f198
spindleredmer <redmer.loen@wearespindle.com>
2015-06-30 15:53:34 +0200

    LILY-995 Editten en deleten van notes

--- frontend/app/utils/controllers/note_edit.js
+++ frontend/app/utils/controllers/note_edit.js
@@ -2,13 +2,14 @@ angular.module('app.utils').controller('EditNoteModalController', EditNoteModalC
 
 EditNoteModalController.$inject = ['$http', '$modalInstance', '$scope', 'note'];
 function EditNoteModalController($http, $modalInstance, $scope, note) {
+    note.modalType = note.type;
     $scope.note = note;
     $scope.ok = function () {
         $http({
             url: '/notes/update/' + $scope.note.id + '/',
             method: 'POST',
             headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-            data: $.param({content: $scope.note.content, type: $scope.note.type})
+            data: $.param({content: $scope.note.content, type: $scope.note.modalType})
         }).success(function() {
             $modalInstance.close($scope.note);
         });

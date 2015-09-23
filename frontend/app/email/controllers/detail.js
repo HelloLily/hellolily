fb0b9b7a8d6ca477e6e2a3b07582264fbfc7adac
ferdy <ferdy@none>
2015-09-23 10:18:48 +0200

    LILY-1027: Show "does not exist"

--- frontend/app/email/controllers/detail.js
+++ frontend/app/email/controllers/detail.js
@@ -13,7 +13,12 @@ function emailConfig ($stateProvider){
         resolve: {
             message: ['EmailMessage', '$stateParams', function(EmailMessage, $stateParams) {
                 var id = $stateParams.id;
-                return EmailMessage.get({id: id}).$promise;
+                return EmailMessage.get({id: id}).$promise.then(function(message) {
+                    return message;
+                }, function() {
+                    // In case the email does not exist, return empty message.
+                    return {};
+                });
             }]
         }
     });
@@ -41,23 +46,25 @@ function EmailDetailController ($scope, $state, $stateParams, EmailMessage, Reci
 
     // Load email body after page resolve has finished,
     // so we can already see email headers before the body is loaded.
-    message.$promise.then(function(result) {
-        if (result.body_html) {
-            result.bodyHTMLUrl = '/messaging/email/html/' + result.id + '/';
-        }else{
-            vm.onlyPlainText = true;
-        }
-        // It's easier to iterate through a single array, so make an array with all recipients
-        vm.message.all_recipients = result.received_by.concat(result.received_by_cc);
-        // Get contacts
-        RecipientInformation.getInformation(vm.message.all_recipients);
+    if (message.id) {
+        message.$promise.then(function(result) {
+            if (result.body_html) {
+                result.bodyHTMLUrl = '/messaging/email/html/' + result.id + '/';
+            }else{
+                vm.onlyPlainText = true;
+            }
+            // It's easier to iterate through a single array, so make an array with all recipients
+            vm.message.all_recipients = result.received_by.concat(result.received_by_cc);
+            // Get contacts
+            RecipientInformation.getInformation(vm.message.all_recipients);
 
-        if (!result.read) {
-            EmailMessage.markAsRead($stateParams.id, true);
-        }
-        // Store current email account
-        SelectedEmailAccount.setCurrentAccountId(vm.message.account);
-    });
+            if (!result.read) {
+                EmailMessage.markAsRead($stateParams.id, true);
+            }
+            // Store current email account
+            SelectedEmailAccount.setCurrentAccountId(vm.message.account);
+        });
+    }
 
     function archiveMessage() {
         EmailMessage.archive({id: vm.message.id}).$promise.then(function () {

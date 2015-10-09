95f0fde80ad2552892220fea7fd219b691c16a2b
Allard Stijnman <a.g.stijnman@gmail.com>
2015-10-09 11:41:22 +0200

    Set default expiry date of case to one week from now

--- lily/cases/forms.py
+++ lily/cases/forms.py
@@ -13,6 +13,7 @@ from lily.parcels.models import Parcel
 from lily.tags.forms import TagsFormMixin
 from lily.tenant.middleware import get_current_user
 from lily.users.models import LilyGroup, LilyUser
+from lily.utils.datetime import week_from_now
 from lily.utils.forms import HelloLilyModelForm
 from lily.utils.forms.widgets import DatePicker, AjaxSelect2Widget
 
@@ -114,7 +115,7 @@ class CreateUpdateCaseForm(TagsFormMixin, HelloLilyModelForm):
         # won't be found for a user. But since it's a required field, an exception is raised.
         user = get_current_user()
         self.fields['assigned_to'].queryset = LilyUser.objects.filter(tenant=user.tenant)
-        self.fields['expires'].initial = datetime.today()
+        self.fields['expires'].initial = week_from_now()
 
         # Pre-select users team if possible
         groups = user.lily_groups.all()

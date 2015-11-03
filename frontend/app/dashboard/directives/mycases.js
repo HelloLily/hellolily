c1ea71a10503a7dcb4c44ca0b1f06364a1d80d27
Ednan Pasagic <ednan.pasagic@wearespindle.com>
2015-11-03 17:27:35 +0100

    LILY-1190: Change behaviour of the 'All' filter in the 'My cases' widget

--- frontend/app/dashboard/directives/mycases.js
+++ frontend/app/dashboard/directives/mycases.js
@@ -35,12 +35,54 @@ function MyCasesController($scope, Case, Cookie) {
     }
 
     function _getMyCases() {
+        var field = '';
+        var sorting = false;
+
+        if (vm.expiresFilter !== '') {
+            field = vm.table.order.column;
+            sorting = vm.table.order.ascending;
+        } else {
+            field = 'expires';
+            sorting = false;
+        }
+
         Case.getMyCasesWidget(
-            vm.table.order.column,
-            vm.table.order.ascending,
+            field,
+            sorting,
             vm.table.expiresFilter
         ).then(function(data) {
-            vm.table.items = data;
+            if (vm.table.expiresFilter !== '') {
+                vm.table.items = data;
+            } else {
+                // ng-repeat sorts keys alphabetically, so for now setup the cases this way.
+                // Angular 1.4 should no longer sort alphabetically, so this could be changed later on.
+                var cases = [
+                    {name: 'Expired', cases: []},
+                    {name: 'Today', cases: []},
+                    {name: 'Tomorrow', cases: []},
+                    {name: 'Later', cases: []},
+                ];
+
+                var now = moment();
+                var tomorrow = moment().add('1', 'day');
+
+                angular.forEach(data, function(myCase) {
+                    var day = moment(myCase.expires);
+
+                    if (day.isBefore(now, 'day')) {
+                        cases[0].cases.push(myCase);
+                    } else if (day.isSame(now, 'day')) {
+                        cases[1].cases.push(myCase);
+                    } else if (day.isSame(tomorrow, 'day')) {
+                        cases[2].cases.push(myCase);
+                    } else {
+                        cases[3].cases.push(myCase);
+                    }
+                });
+
+                vm.table.items = cases;
+            }
+
             vm.highPrioCases = 0;
             for (var i in data) {
                 if (data[i].priority === 3) {

0c6ea82d1f6a88e8b37f5dc5c00696a8474dfbc7
Arjen Vellinga <arjen.vellinga@wearespindle.com>
2016-05-25 10:54:33 +0200

    improved admin interfaces

--- lily/utils/admin.py
+++ lily/utils/admin.py
@@ -2,7 +2,17 @@ from django.contrib import admin
 
 from .models.models import PhoneNumber, Address, EmailAddress, ExternalAppLink
 
-admin.site.register(PhoneNumber)
+
+@admin.register(PhoneNumber)
+class PhoneNumberAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'number',
+        'status',
+        'type',
+        'other_type',
+    )
+
 admin.site.register(Address)
 admin.site.register(EmailAddress)
 admin.site.register(ExternalAppLink)

6be19a8f4017e9ff9a193d019bff80ed0557e0d1
Ednan Pasagic <ednan.pasagic@wearespindle.com>
2016-11-23 14:38:51 +0100

    Added missing . for email template variable create/update

--- frontend/app/preferences/email/controllers/templatevariable_createupdate.js
+++ frontend/app/preferences/email/controllers/templatevariable_createupdate.js
@@ -34,7 +34,8 @@ function preferencesConfig($stateProvider) {
 
 angular.module('app.preferences').controller('PreferencesTemplateVariablesCreateUpdate', PreferencesTemplateVariablesCreateUpdate);
 
-function PreferencesTemplateVariablesCreateUpdate() {
+PreferencesTemplateVariablesCreateUpdate.$inject = ['$scope'];
+function PreferencesTemplateVariablesCreateUpdate($scope) {
     HLInbox.init({textEditorId: 'id_text'});
     HLInbox.initWysihtml5();
     HLEmailTemplates.init({
@@ -42,5 +43,11 @@ function PreferencesTemplateVariablesCreateUpdate() {
         openVariable: '[[',
         closeVariable: ']]',
         textEditorId: '#id_text',
+    });
+
+    // Listen to Angular broadcast function on scope destroy.
+    $scope.$on('$destroy', function() {
+        // Properly destroy the rich text editor to prevent memory leaks.
+        HLInbox.destroyEditor();
     });
 }

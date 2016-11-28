7293ce7f12a127944e453b79fa6c0f65b78fe226
Jukka Koivunen <jjkoivun@gmail.com>
2016-11-28 15:57:34 +0100

    LILY-1708: Wrap string parameters in quotes since they otherwise seem to not work properly.

--- ci/indexing_needed.sh
+++ ci/indexing_needed.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ $(git log ${TRAVIS_COMMIT_RANGE} --format=oneline -- **/search.py | wc -l) -gt 0 ]; then
+if [ $(git log ${TRAVIS_COMMIT_RANGE} --format=oneline -- '**/search.py' | wc -l) -gt 0 ]; then
     echo "true"
 else
     echo "false"

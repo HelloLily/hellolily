9497b3324cec43d2ffd8cb7a9dbaacd81108cfad
Ednan Pasagic <ednan.pasagic@wearespindle.com>
2017-04-13 10:19:51 +0200

    LILY-2075: Automatically add case on tenant creation

--- lily/messaging/email/api/serializers.py
+++ lily/messaging/email/api/serializers.py
@@ -1,7 +1,10 @@
+from django.conf import settings
+from django.contrib.sites.models import Site
 from django.db.models import Q
 from django.utils.translation import ugettext_lazy as _
 from oauth2client.contrib.django_orm import Storage
 from rest_framework import serializers
+from templated_email import send_templated_mail
 
 from lily.api.fields import DynamicQuerySetPrimaryKeyRelatedField
 from lily.api.nested.mixins import RelatedSerializerMixin
@@ -178,6 +181,26 @@ class EmailAccountSerializer(WritableNestedSerializer):
         if user.info.email_account_status == UserInfo.INCOMPLETE:
             user.info.email_account_status = UserInfo.COMPLETE
             user.info.save()
+
+            # Send a mail to the first email account in the tenant.
+            if EmailAccount.objects.count() == 1:
+                # Get the current site.
+                try:
+                    current_site = Site.objects.get_current()
+                except Site.DoesNotExist:
+                    current_site = 'Lily'
+
+                send_templated_mail(
+                    template_name='first_impressions',
+                    recipient_list=[instance.email_address],
+                    context={
+                        'current_site': current_site,
+                        'first_name': user.first_name,
+                    },
+                    from_email=settings.EMAIL_PERSONAL_HOST_USER,
+                    auth_user=settings.EMAIL_PERSONAL_HOST_USER,
+                    auth_password=settings.EMAIL_PERSONAL_HOST_PASSWORD
+                )
 
         return super(EmailAccountSerializer, self).update(instance, validated_data)
 

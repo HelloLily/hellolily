4a0679f302ce54c2d6d3e806f7a8ceeefe1ec404
Arjen Vellinga <arjen.vellinga@wearespindle.com>
2017-06-13 10:46:22 +0200

    LILY-2447 Improved download new email message by less db access and less indexing

--- lily/messaging/email/builders/message.py
+++ lily/messaging/email/builders/message.py
@@ -10,7 +10,7 @@ from bs4 import BeautifulSoup, UnicodeDammit
 from dateutil.parser import parse
 from django.conf import settings
 from django.core.files import File
-from django.db import transaction, IntegrityError
+from django.db import transaction
 import pytz
 
 from python_imap.utils import get_extensions_for_type
@@ -111,13 +111,32 @@ class MessageBuilder(object):
         self.get_or_create_message({'id': message_id})
         self.message.thread_id = message_info['threadId']
 
-        # UNREAD identifier check to see if message is read
+        # UNREAD identifier check to see if message is read.
         self.message.read = settings.GMAIL_LABEL_UNREAD not in message_info.get('labelIds', [])
 
-        # Store all labels
-        for label in message_info.get('labelIds', []):
-            db_label = self.manager.get_label(label)
-            self.labels.append(db_label)
+        # Get the available Label objects for the message from the database and the missing ones by the API.
+        # First, get all labels from the database.
+        db_labels = self.manager.email_account.labels.all()
+
+        message_label_set = set(label for label in message_info.get('labelIds', []))
+        db_label_set = set(label.label_id for label in db_labels)
+
+        # Use set operations to get the available and missing labels.
+        # Labels that exist in Gmail for this message but are not in our db.
+        missing_label_ids = message_label_set - db_label_set
+        if missing_label_ids:
+            # Labels missing in our database should be retrieved by the API.
+            for label_id in missing_label_ids:
+                db_label = self.manager.get_label(label_id, use_db=False)
+                self.labels.append(db_label)
+
+        # Labels that exist both in Gmail and in our db. Those labels are already retrieved from the db above.
+        available_label_ids = message_label_set & db_label_set
+        if available_label_ids:
+            # Use dict comprehension to get a dictonary with the label_id as the keys and the label as the value.
+            db_label_dict = {label.label_id: label for label in db_labels}
+            for label_id in available_label_ids:
+                self.labels.append(db_label_dict[label_id])
 
     def _save_message_payload(self, payload):
         """
@@ -166,10 +185,11 @@ class MessageBuilder(object):
             elif header_name in ['to', 'from', 'cc', 'delivered-to']:
                 self._create_recipients(header_name, header_value)
             else:
-                self.headers.append(EmailHeader(
-                    name=header['name'],
-                    value=header['value'],
-                ))
+                if header_name.startswith('message-id') or header_name.startswith('reply-to'):
+                    self.headers.append(EmailHeader(
+                        name=header['name'],
+                        value=header['value'],
+                    ))
 
     def _parse_message_part(self, part):
         """
@@ -178,23 +198,23 @@ class MessageBuilder(object):
         Args:
             part: dict with message part
         """
-        # Check if part has child parts
+        # Check if part has child parts.
         if 'parts' in part:
             for part in part['parts']:
                 self._parse_message_part(part)
         else:
             part_headers = self._headers_to_dict(part.get('headers', {}))
 
-            # Get content type
+            # Get content type.
             mime_type = part['mimeType']
 
-            # Check if part is an attachment
+            # Check if part is an attachment.
             if part['filename'] or 'data' not in part['body'] or mime_type == 'text/css':
                 self._create_attachment(part, part_headers)
 
-            # Handle non attachment
+            # Handle non attachment.
             else:
-                # Decode body part
+                # Decode body part.
                 body = base64.urlsafe_b64decode(part['body']['data'].encode())
 
                 encoding = None
@@ -208,10 +228,10 @@ class MessageBuilder(object):
                     self._create_body_text(body, encoding)
 
                 elif mime_type == 'text/xml':
-                    # Conversation xml, do not store
+                    # Conversation xml, do not store.
                     pass
                 elif mime_type == 'text/rfc822-headers':
-                    # Header part, not needed
+                    # Header part, not needed.
                     pass
                 elif mime_type in (
                         'text/css',
@@ -222,7 +242,7 @@ class MessageBuilder(object):
                         'image/png',
                         'image/jpeg',
                 ):
-                    # attachments
+                    # Attachments.
                     self._create_attachment(part, part_headers)
                 else:
                     self._create_attachment(part, part_headers)
@@ -234,24 +254,24 @@ class MessageBuilder(object):
 
     def _create_attachment(self, part, headers):
         """
-        Create an attachment for the given part
+        Create an attachment for the given part.
 
         Args:
             part (dict): with attachment info
             headers (dict): headers for message part
 
         Raises:
-            Attachment exception if attachment couldn't be created
+            Attachment exception if attachment couldn't be created.
         """
         logger.debug('Storing attachment for message %s' % self.message.message_id)
         headers = {name.lower(): value for name, value in headers.iteritems()}
 
-        # Check if attachment is inline
+        # Check if attachment is inline.
         inline = False
         if headers and headers.get('content-id', False):
             inline = True
 
-        # Get file data from part or from remote
+        # Get file data from part or from remote.
         if 'data' in part['body']:
             file_data = part['body']['data']
         elif 'attachmentId' in part['body']:
@@ -267,7 +287,7 @@ class MessageBuilder(object):
 
         file_data = base64.urlsafe_b64decode(file_data.encode('UTF-8'))
 
-        # create as string file
+        # Create as string file.
         file = StringIO.StringIO(file_data)
         if headers and 'content-type' in headers:
             file.content_type = headers['content-type'].split(';')[0]
@@ -279,7 +299,7 @@ class MessageBuilder(object):
         if len(file.name) > 200:
             file.name = None
 
-        # No filename in part, create a name
+        # No filename in part, create a name.
         if not file.name:
             extensions = get_extensions_for_type(file.content_type)
             if part.get('partId'):
@@ -293,14 +313,14 @@ class MessageBuilder(object):
 
         final_file = File(file, file.name)
 
-        # Create a EmailAttachment object
+        # Create a EmailAttachment object.
         attachment = EmailAttachment()
         attachment.attachment = final_file
         attachment.size = file.size
         attachment.inline = inline
         attachment.tenant_id = self.manager.email_account.tenant_id
 
-        # Check if inline attachment
+        # Check if inline attachment.
         if inline:
             attachment.cid = headers.get('content-id')
 
@@ -420,21 +440,15 @@ class MessageBuilder(object):
         # Only save if there is a sent date, otherwise it's a chat message.
         if self.message.sent_date and self.message.sender_id:
 
+            self.message.skip_signal = True  # Disable intermediate reindexing of the message.
+
             # Check for attachments.
             if self.attachments or self.inline_attachments:
                 self.message.has_attachment = True
 
             if not self.message.pk:
                 # Save before we can add many-to-many and foreign keys.
-                try:
-                    self.message.save()
-                except IntegrityError:
-                    existing_message = EmailMessage.objects.get(
-                        account_id=self.message.account_id,
-                        message_id=self.message.message_id,
-                    )
-                    self.message.id = existing_message.id
-                    self.message.save()
+                self.message.save()
 
             # Save recipients.
             self.message.received_by.add(*self.received_by)
@@ -457,9 +471,9 @@ class MessageBuilder(object):
             # Save attachments.
             if len(self.attachments):
                 self.message.attachments.all().delete()
+                self.message.attachments.add(*self.attachments)
 
-            self.message.attachments.add(*self.attachments)
-
+            self.message.skip_signal = False  # Re-enable indexing of the email on the last save.
             self.message.save()
         else:
             logger.warning('Downloaded a message other than an email.')

6683cdc6dfde7a3174cde5664a4c92547e5ddff4
Ednan Pasagic <ednan.pasagic@wearespindle.com>
2017-07-04 11:06:55 +0200

    LILY-2512: Add extra response format for internal number search

--- lily/search/functions.py
+++ lily/search/functions.py
@@ -3,25 +3,28 @@ from lily.contacts.models import Contact
 from lily.utils.functions import parse_phone_number
 
 
-def search_number(tenant_id, number):
+def search_number(tenant_id, number, return_related=True):
     """
     If the phone number belongs to an account, this returns the first account and all its contacts
     Else if the number belongs to a contact, this returns the first contact and all its accounts
     """
-
     phone_number = parse_phone_number(number)
     accounts = Account.objects.filter(phone_numbers__number=phone_number, is_deleted=False)
     contacts = Contact.objects.filter(phone_numbers__number=phone_number, is_deleted=False)
 
+    accounts_result = []
+    contacts_result = []
+
     if accounts:
         accounts_result = [accounts[0]]
-        contacts_result = accounts[0].contacts.all()
+
+        if return_related:
+            contacts_result = accounts[0].contacts.filter(is_deleted=False)
     elif contacts:
         contacts_result = [contacts[0]]
-        accounts_result = contacts[0].accounts.all()
-    else:
-        accounts_result = []
-        contacts_result = []
+
+        if return_related:
+            accounts_result = contacts[0].accounts.filter(is_deleted=False)
 
     return {
         'data': {

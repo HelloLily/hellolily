c77840f6388689ac403376df4d8710332ae7301b
Allard Stijnman <a.g.stijnman@gmail.com>
2017-10-18 16:59:02 +0200

    Fix the call notification migration

--- lily/calls/migrations/0006_auto_20171011_0935.py
+++ lily/calls/migrations/0006_auto_20171011_0935.py
@@ -5,6 +5,7 @@ import random
 import string
 
 import logging
+
 from django.db import migrations, models
 
 
@@ -19,11 +20,20 @@ def migrate_calls(apps, schema_editor):
     Call = apps.get_model('calls', 'Call')
     CallRecord = apps.get_model('calls', 'CallRecord')
     CallParticipant = apps.get_model('calls', 'CallParticipant')
-    LilyUser = apps.get_model('users', 'LilyUser')
-    Contact = apps.get_model('contacts', 'Contact')
-    Account = apps.get_model('accounts', 'Account')
+    Note = apps.get_model('notes', 'Note')
+    ContentType = apps.get_model('contenttypes', 'ContentType')
 
-    for call in Call.objects.all():
+    call_ctype = ContentType.objects.get(app_label='calls', model='call')
+    cr_ctype = ContentType.objects.get(app_label='calls', model='callrecord')
+
+    calls = Call.objects.filter(created__isnull=False)
+
+    print 'number of calls to migrate: %s' % len(calls)
+
+    for index, call in enumerate(calls):
+        if index % 100 == 0:
+            print 'Another 100 done! We\'re at %s' % index
+
         caller = CallParticipant.objects.get_or_create(
             tenant=call.tenant,
             name=call.caller_name,
@@ -31,46 +41,40 @@ def migrate_calls(apps, schema_editor):
             internal_number=None
         )[0]
 
-        destination_name = ''
-        user = LilyUser.objects.filter(internal_number=call.internal_number).first()
-        if user:
-            destination_name = u' '.join([user.first_name, user.last_name]).strip()
-        else:
-            contact = Contact.objects.filter(phone_numbers__number=call.called_number).first()
-
-            if contact:
-                destination_name = ' '.join([contact.first_name, contact.last_name]).strip()
-            else:
-                account = Account.objects.filter(phone_numbers__number=call.called_number).first()
-
-                if account:
-                    destination_name = account.name
-
         destination = CallParticipant.objects.get_or_create(
             tenant=call.tenant,
-            name=destination_name,
+            name='',
             number=call.called_number,
             internal_number=call.internal_number
         )[0]
 
         unique_id = call.unique_id
-        if unique_id == '90000':
+        call_id_exists = CallRecord.objects.filter(call_id=unique_id).exists()
+        while call_id_exists:
             unique_id = ''.join(random.choice(string.lowercase) for x in range(100))
+            call_id_exists = CallRecord.objects.filter(call_id=unique_id).exists()
 
-        if call.created:
-            try:
-                CallRecord.objects.create(
-                    tenant=call.tenant,
-                    call_id=unique_id,
-                    start=call.created,
-                    end=None,
-                    status=ENDED,
-                    direction=INBOUND,
-                    caller=caller,
-                    destination=destination
-                )
-            except:
-                logger.exception('Migration failed for call %s' % call.id)
+        cr = CallRecord.objects.create(
+            tenant=call.tenant,
+            call_id=unique_id,
+            start=call.created,
+            end=None,
+            status=ENDED,
+            direction=INBOUND,
+            caller=caller,
+            destination=destination
+        )
+
+        for note in Note.objects.filter(content_type=call_ctype, object_id=call.id):
+            Note.objects.create(
+                tenant_id=note.tenant_id,
+                content=note.content,
+                author_id=note.author_id,
+                type=note.type,
+                is_pinned=note.is_pinned,
+                content_type=cr_ctype,
+                object_id=cr.id
+            )
 
 
 def do_nothing(apps, schema_editor):
@@ -81,9 +85,8 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('calls', '0005_callparticipant_callrecord_calltransfer'),
-        ('users', '0023_userinvite'),
-        ('contacts', '0013_auto_20170717_2005'),
-        ('accounts', '0019_auto_20170419_0926'),
+        ('notes', '0011_auto_20161125_1559'),
+        ('contenttypes', '0002_remove_content_type_name'),
     ]
 
     operations = [

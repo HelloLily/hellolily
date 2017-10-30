561dc1de45d76eab509ff9931ea0a51d70778c6f
Ednan Pasagic <ednan.pasagic@wearespindle.com>
2017-10-30 10:53:34 +0100

    LILY-2722: Improve auto fill for phone numbers in email supercard

--- lily/messaging/email/api/views.py
+++ lily/messaging/email/api/views.py
@@ -15,6 +15,8 @@ from lily.messaging.email.utils import get_email_parameter_api_dict, reindex_ema
 from lily.search.lily_search import LilySearch
 from lily.users.models import UserInfo
 from lily.users.api.serializers import LilyUserSerializer
+from lily.utils.functions import format_phone_number
+from lily.utils.models.models import PhoneNumber
 
 from .serializers import (EmailLabelSerializer, EmailAccountSerializer, EmailMessageSerializer,
                           EmailTemplateFolderSerializer, EmailTemplateSerializer, SharedEmailConfigSerializer,
@@ -352,7 +354,9 @@ class EmailMessageViewSet(mixins.RetrieveModelMixin,
     @detail_route(methods=['post'])
     def extract(self, request, pk=None):
         """
-        Attempt to extract phone numbers from the given email message. If account is given it will
+        Attempt to extract phone numbers from the given email message.
+        If an account is given it will attempt to use that account's country.
+        Otherwise it will use the user's tenant's country.
         """
         email = self.get_object()
         account = request.data['account']
@@ -379,8 +383,11 @@ class EmailMessageViewSet(mixins.RetrieveModelMixin,
                 number = match.number
 
                 number = phonenumbers.format_number(number, number_format).replace(' ', '')
+                number = format_phone_number(number, country, True)
 
-                if number not in phone_numbers:
+                number_exists = PhoneNumber.objects.filter(number=number, status=PhoneNumber.ACTIVE_STATUS).exists()
+
+                if number not in phone_numbers and not number_exists:
                     phone_numbers.append(number)
 
         return Response({'phone_numbers': phone_numbers})

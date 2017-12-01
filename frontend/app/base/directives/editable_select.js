d6151655091cbc93efac8f36f3132fe7b6da6add
Ednan Pasagic <ednan.pasagic@wearespindle.com>
2017-12-01 11:56:54 +0100

    LILY-2776: Prevent inline form opening when social media link clicked

--- frontend/app/base/directives/editable_select.js
+++ frontend/app/base/directives/editable_select.js
@@ -26,16 +26,6 @@ function editableSelect() {
         controllerAs: 'es',
         transclude: true,
         bindToController: true,
-        link: (scope, element, attr) => {
-            // Bind click event to the current directive.
-            element.on('click', '.editable-click', () => {
-                if (scope.es.search) {
-                    scope.es[scope.es.formName].$show();
-
-                    scope.$apply();
-                }
-            });
-        },
     };
 }
 

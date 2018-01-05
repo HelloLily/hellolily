7634cb855b05306be87ec5059ef844d1791210d4
Jukka Koivunen <jjkoivun@gmail.com>
2018-01-05 15:42:07 +0100

    LILY-2820: Use pytest instead of nose or django test runner.

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-COVERAGE_PROCESS_START=./.coveragerc docker-compose -f docker-compose.yml -f docker-compose.new-build.yml run --rm --service-ports web bash -c "Dockers/wait-for db:5432 && coverage run --source='.' --parallel-mode manage.py test --parallel=2"
+docker-compose -f docker-compose.yml -f docker-compose.new-build.yml run --rm --service-ports web bash -c "Dockers/wait-for db:5432 && pytest"
--- lily/tests/runner.py
+++ lily/tests/runner.py
@@ -5,7 +5,7 @@ from django.test.runner import DiscoverRunner
 from django.conf import settings
 
 
-class LilyNoseTestSuiteRunner(DiscoverRunner):
+class LilyNoseTestSuiteRunner(object):
     """
     Bootstrap into the testsuite running process.
 
@@ -13,8 +13,10 @@ class LilyNoseTestSuiteRunner(DiscoverRunner):
     Settings it changes:
         * TESTING=True, useful to check if we are running tests.
     """
-    def __init__(self, *args, **kwargs):
-        super(LilyNoseTestSuiteRunner, self).__init__(*args, **kwargs)
+    def __init__(self, verbosity=1, failfast=False, keepdb=False, *args, **kwargs):
+        self.verbosity = verbosity
+        self.failfast = failfast
+        self.keepdb = keepdb
 
         if settings.TEST_SUPPRESS_LOG:
             # Suppress logging during a testrun.
@@ -25,3 +27,25 @@ class LilyNoseTestSuiteRunner(DiscoverRunner):
         # manage.py test already does this, but not when providing a path, like
         # manage.py test lily/contacts/tests.
         settings.DEBUG = False
+
+    def run_tests(self, test_labels):
+        """Run pytest and return the exitcode.
+
+        It translates some of Django's test command option to pytest's.
+        """
+        import pytest
+
+        argv = []
+        if self.verbosity == 0:
+            argv.append('--quiet')
+        if self.verbosity == 2:
+            argv.append('--verbose')
+        if self.verbosity == 3:
+            argv.append('-vv')
+        if self.failfast:
+            argv.append('--exitfirst')
+        if self.keepdb:
+            argv.append('--reuse-db')
+
+        argv.extend(test_labels)
+        return pytest.main(argv)

72a782414690a5e3f134ba7054b913187704a8f5
Jukka Koivunen <jjkoivun@gmail.com>
2018-01-08 09:03:40 +0100

    LILY-2820: Add parallel testing and coverage plugins to pytest.

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker-compose -f docker-compose.yml -f docker-compose.new-build.yml run --rm --service-ports web bash -c "Dockers/wait-for db:5432 && pytest"
+docker-compose -f docker-compose.yml -f docker-compose.new-build.yml run --rm --service-ports web bash -c "Dockers/wait-for db:5432 && pytest -n auto --cov=lily"

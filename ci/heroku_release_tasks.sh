d96f12f4c41157d7a2a882517705d4e5f551d762
Jukka Koivunen <jjkoivun@gmail.com>
2018-05-11 17:19:01 +0200

    LILY-3043: Heroku CLI doesn't work inside heroku it seems, use API instead.

--- ci/heroku_release_tasks.sh
+++ ci/heroku_release_tasks.sh
@@ -9,13 +9,13 @@ then
     exit 0
 else
     echo "Migrations needed, setting Heroku app to maintenance mode."
-    heroku maintenance:on
+    python ./ci/patch_heroku_app.py ${HEROKU_APP_NAME} ${HEROKU_API_KEY} maintenance true
     echo "Scaling down beat dynos to 0."
-    heroku ps:scale beat=0
+    python ./ci/patch_heroku_app.py ${HEROKU_APP_NAME}/formation/beat ${HEROKU_API_KEY} quantity 0
     echo "Running migrations."
     python manage.py migrate
     echo "Migrations done, switching maintenance mode off."
-    heroku maintenance:off
+    python ./ci/patch_heroku_app.py ${HEROKU_APP_NAME} ${HEROKU_API_KEY} maintenance false
     echo "Scaling beat dynos up back to 1."
-    heroku ps:scale beat=1
+    python ./ci/patch_heroku_app.py ${HEROKU_APP_NAME}/formation/beat ${HEROKU_API_KEY} quantity 1
 fi

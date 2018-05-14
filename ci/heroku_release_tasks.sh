ad03862bec8ab885937145fc70e7b4bd60a9ba69
Jukka Koivunen <jjkoivun@gmail.com>
2018-05-14 16:46:02 +0200

    LILY-3043: Correct separate exit statement however does help.

--- ci/heroku_release_tasks.sh
+++ ci/heroku_release_tasks.sh
@@ -8,10 +8,12 @@ if [ $(python manage.py showmigrations | grep '\[ \]' | wc -l) -gt 0 ]; then
     python ./ci/patch_heroku_app.py ${HEROKU_APP_NAME}/formation/beat ${HEROKU_API_KEY} quantity 0
     echo "Running migrations."
     python manage.py migrate
+    migration_result=$?
     echo "Migrations done, switching maintenance mode off."
     python ./ci/patch_heroku_app.py ${HEROKU_APP_NAME} ${HEROKU_API_KEY} maintenance false
     echo "Scaling beat dynos up back to 1."
     python ./ci/patch_heroku_app.py ${HEROKU_APP_NAME}/formation/beat ${HEROKU_API_KEY} quantity 1
+    exit $migration_result
 else
     echo "No Migration needed, proceeding with the deployment."
 fi

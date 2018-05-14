2bd17043bfe7cb3e600c9bc324cc30741330da02
Jukka Koivunen <jjkoivun@gmail.com>
2018-05-14 14:39:53 +0200

    LILY-3043: Fixed the migration detection command and added a print when migrations are not needed.

--- ci/heroku_release_tasks.sh
+++ ci/heroku_release_tasks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run migrations if needed.
-if [ $(docker-compose run --rm web python manage.py showmigrations | grep '\[ \]' | wc -l) -gt 0 ]; then
+if [ $(python manage.py showmigrations | grep '\[ \]' | wc -l) -gt 0 ]; then
     echo "Migrations needed, setting Heroku app to maintenance mode."
     python ./ci/patch_heroku_app.py ${HEROKU_APP_NAME} ${HEROKU_API_KEY} maintenance true
     echo "Scaling down beat dynos to 0."
@@ -12,4 +12,6 @@ if [ $(docker-compose run --rm web python manage.py showmigrations | grep '\[ \]
     python ./ci/patch_heroku_app.py ${HEROKU_APP_NAME} ${HEROKU_API_KEY} maintenance false
     echo "Scaling beat dynos up back to 1."
     python ./ci/patch_heroku_app.py ${HEROKU_APP_NAME}/formation/beat ${HEROKU_API_KEY} quantity 1
+else
+    echo "No Migration needed, proceeding with the deployment."
 fi

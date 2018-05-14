8fb79ea078d9a4ca2167555f41248c4e9bda13ec
Jukka Koivunen <jjkoivun@gmail.com>
2018-05-14 16:13:34 +0200

    LILY-3043: Exit the release phase script with the migration command result.

--- ci/heroku_release_tasks.sh
+++ ci/heroku_release_tasks.sh
@@ -6,12 +6,14 @@ if [ $(python manage.py showmigrations | grep '\[ \]' | wc -l) -gt 0 ]; then
     python ./ci/patch_heroku_app.py ${HEROKU_APP_NAME} ${HEROKU_API_KEY} maintenance true
     echo "Scaling down beat dynos to 0."
     python ./ci/patch_heroku_app.py ${HEROKU_APP_NAME}/formation/beat ${HEROKU_API_KEY} quantity 0
+    migration_result=$?
     echo "Running migrations."
     python manage.py migrate
     echo "Migrations done, switching maintenance mode off."
     python ./ci/patch_heroku_app.py ${HEROKU_APP_NAME} ${HEROKU_API_KEY} maintenance false
     echo "Scaling beat dynos up back to 1."
     python ./ci/patch_heroku_app.py ${HEROKU_APP_NAME}/formation/beat ${HEROKU_API_KEY} quantity 1
+    exit migration_result
 else
     echo "No Migration needed, proceeding with the deployment."
 fi

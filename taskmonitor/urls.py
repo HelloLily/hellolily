809ba815810874ec215fd3f65707d17f2d8a77c8
Allard Stijnman <a.g.stijnman@gmail.com>
2018-10-15 14:05:08 +0200

    Remove taskmonitor models

--- taskmonitor/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-
-from taskmonitor.views import TaskStatusView
-
-urlpatterns = [
-    url(r'^task/status/(?P<task_id>[\w-]+)/$', TaskStatusView.as_view(), name='task_status'),
-]

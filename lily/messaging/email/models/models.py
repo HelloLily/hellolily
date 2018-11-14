46f31943d39e9bce15e53d4caec7a915c24e75b1
Ard Timmerman <ardtimmerman@gmail.com>
2018-11-14 12:02:29 +0100

    Revert "LILY-3209: Now switching email to/from if mail is being sent"
    This reverts commit ef4c5b84e7a8064611a2836718ba3c7b1d4c2f4e.

--- lily/messaging/email/models/models.py
+++ lily/messaging/email/models/models.py
@@ -5,6 +5,7 @@ from email.mime.audio import MIMEAudio
 from email.mime.base import MIMEBase
 from email.mime.image import MIMEImage
 from email.mime.text import MIMEText
+from email.utils import parseaddr
 import logging
 import mimetypes
 import os
@@ -317,13 +318,15 @@ class EmailMessage(models.Model):
 
     @property
     def reply_to(self):
-        from ..utils import get_reply_to_email_from_headers
         """
         Return the reply to address if it is present as a header, otherwise use email address of the sender.
         """
-        email = get_reply_to_email_from_headers(self.headers)
-        if email:
-            return email
+        header = self.headers.filter(name__istartswith='reply-to').first()
+        if header:
+            # A reply-to header can contain a plain email address, an email address enclosed by brackets,
+            # or has a "name" <foo@bar.com> construction.
+            # So return only the email address instead of the actual header value.
+            return parseaddr(header.value)[1]
         else:
             return self.sender.email_address
 

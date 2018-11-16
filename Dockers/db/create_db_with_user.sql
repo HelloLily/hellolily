e4d07644f44c77d321d2d37772639e06dfe4cab8
Allard Stijnman <a.g.stijnman@gmail.com>
2018-11-16 15:21:07 +0100

    Use environment variables for db init

--- Dockers/db/create_db_with_user.sql
+++
@@ -1,4 +0,0 @@
-CREATE USER hellolily;
-CREATE DATABASE hellolily;
-GRANT ALL PRIVILEGES ON DATABASE hellolily TO hellolily;
-ALTER USER hellolily CREATEDB;
--- Dockers/db/init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Grant hellolily access from all IPs."
-echo "host all hellolily 0.0.0.0/0 trust" >> /var/lib/postgresql/data/pg_hba.conf
-
-echo "Done initializing."

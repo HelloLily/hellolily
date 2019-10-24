67b905dc668bf11bf4857eea3a52a1216476acb4
Allard Stijnman <a.g.stijnman@gmail.com>
2019-10-24 14:34:00 +0200

    Fix account address

--- lily/hubspot/management/commands/export_accounts.py
+++ lily/hubspot/management/commands/export_accounts.py
@@ -9,34 +9,41 @@ from django.core.paginator import Paginator
 from lily.accounts.models import Account, Website
 from lily.hubspot.prefetch_objects import website_prefetch, phone_prefetch, social_media_prefetch, \
     addresses_prefetch, tags_prefetch, pinned_notes_prefetch
-from lily.hubspot.utils import _s, _strip_website, get_mappings, get_phone_numbers
-from lily.socialmedia.models import SocialMedia
+from lily.hubspot.utils import _s, _strip_website  # , get_mappings  # , get_phone_numbers
+# from lily.socialmedia.models import SocialMedia
 from lily.tenant.middleware import set_current_user
-from lily.tenant.models import Tenant
+# from lily.tenant.models import Tenant
 from lily.users.models import LilyUser
-
+from lily.utils.models.models import Address
 
 field_names = (
     'account_id',
 
-    'name',
-    'description',
+    # 'name',
+    # 'description',
     'domain',
-    'owner',
-    'type',
-    'phone',
+    # 'owner',
+    # 'type',
 
-    'twitterhandle',
-    'linkedin_company_page',
-    'facebook_company_page',
+    # 'phone',
 
-    'freedom_url',
-    'entity',
+    'street',
+    'city',
+    'state',
+    'zip',
+    'country',
 
-    'pinned_notes',
+    # 'twitterhandle',
+    # 'linkedin_company_page',
+    # 'facebook_company_page',
 
-    'lily_created',
-    'lily_modified',
+    # 'freedom_url',
+    # 'entity',
+
+    # 'pinned_notes',
+
+    # 'lily_created',
+    # 'lily_modified',
 )
 
 
@@ -49,8 +56,8 @@ class Command(BaseCommand):
     def handle(self, tenant_id, *args, **options):
         self.stdout.write(self.style.SUCCESS('>>') + '  Starting with accounts export')
         set_current_user(LilyUser.objects.filter(tenant_id=tenant_id, is_active=True).first())
-        tenant = Tenant.objects.get(id=tenant_id)
-        m = get_mappings(tenant_id)
+        # tenant = Tenant.objects.get(id=tenant_id)
+        # m = get_mappings(tenant_id)
 
         csvfile = StringIO.StringIO()
         writer = csv.DictWriter(csvfile, fieldnames=field_names)
@@ -79,9 +86,10 @@ class Command(BaseCommand):
             self.stdout.write('    Page: {} / {}'.format(page_number, paginator.num_pages))
             for account in account_list:
                 website = account.prefetched_websites[0] if account.prefetched_websites else Website()
-                phone_numbers = get_phone_numbers(account, tenant)
-                social_media = {social.name: social for social in account.prefetched_social_media}
-                tags = [tag.name for tag in account.prefetched_tags]
+                address = account.prefetched_addresses[0] if account.prefetched_addresses else Address()
+                # phone_numbers = get_phone_numbers(account, tenant)
+                # social_media = {social.name: social for social in account.prefetched_social_media}
+                # tags = [tag.name for tag in account.prefetched_tags]
 
                 pinned_notes = ''
                 for note in account.prefetched_pinned_notes:
@@ -93,31 +101,40 @@ class Command(BaseCommand):
 
                     pinned_notes += content
 
-                f_url = 'https://freedom.voys.nl/client/{}'.format(account.customer_id) if account.customer_id else ''
-                data = {
-                    'account_id': _s(account.pk),
+                if address.address:
+                    # f_url = 'https://freedom.voys.nl/client/{}'.format(
+                    #     account.customer_id
+                    # ) if account.customer_id else ''
+                    data = {
+                        'account_id': _s(account.pk),
 
-                    'name': _s(account.name),
-                    'description': _s(account.description),
-                    'domain': _s(_strip_website(website.website)),
-                    'owner': _s(m.lilyuser_to_owner_mapping.get(account.assigned_to_id, '')),
-                    'type': _s(m.account_status_to_company_type_mapping[account.status_id]),
+                        # 'name': _s(account.name),
+                        # 'description': _s(account.description),
+                        'domain': _s(_strip_website(website.website)),
+                        # 'owner': _s(m.lilyuser_to_owner_mapping.get(account.assigned_to_id, '')),
+                        # 'type': _s(m.account_status_to_company_type_mapping[account.status_id]),
 
-                    'phone': _s(phone_numbers),
+                        # 'phone': _s(phone_numbers),
 
-                    'twitterhandle': _s(social_media.get('twitter', SocialMedia()).username or ''),
-                    'linkedin_company_page': _s(social_media.get('linkedin', SocialMedia()).profile_url or ''),
-                    'facebook_company_page': _s(social_media.get('facebook', SocialMedia()).profile_url or ''),
+                        'street': _s(address.address),
+                        'city': _s(address.city),
+                        'state': _s(address.state_province),
+                        'zip': _s(address.postal_code),
+                        'country': _s(address.country),
 
-                    'freedom_url': _s(f_url),
-                    'entity': _s('c_entity_VoysBE' if u'belgië' in tags else 'c_entity_VoysNL'),
+                        # 'twitterhandle': _s(social_media.get('twitter', SocialMedia()).username or ''),
+                        # 'linkedin_company_page': _s(social_media.get('linkedin', SocialMedia()).profile_url or ''),
+                        # 'facebook_company_page': _s(social_media.get('facebook', SocialMedia()).profile_url or ''),
 
-                    'pinned_notes': pinned_notes,
+                        # 'freedom_url': _s(f_url),
+                        # 'entity': _s('c_entity_VoysBE' if u'belgië' in tags else 'c_entity_VoysNL'),
 
-                    'lily_created': _s(account.created.strftime("%d %b %Y - %H:%M:%S")),
-                    'lily_modified': _s(account.modified.strftime("%d %b %Y - %H:%M:%S")),
-                }
-                writer.writerow(data)
+                        # 'pinned_notes': pinned_notes,
+
+                        # 'lily_created': _s(account.created.strftime("%d %b %Y - %H:%M:%S")),
+                        # 'lily_modified': _s(account.modified.strftime("%d %b %Y - %H:%M:%S")),
+                    }
+                    writer.writerow(data)
 
         filename = 'exports/tenant_{}/companies.csv'.format(tenant_id)
         default_storage.save(name=filename, content=csvfile)
